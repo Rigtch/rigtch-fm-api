cbb88aee50e1c439b5423994bb2b3c8c00fc1dc6
Igor <makowskiigor@gmail.com>
2022-11-03 14:46:38 +0100

    feat(auth-service): create login method

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -1,7 +1,20 @@
 import { Injectable } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Profile } from 'passport-spotify'
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
+
+  login({ id, username }: Profile) {
+    const payload = {
+      name: username,
+      sub: id,
+    }
+
+    return this.jwtService.sign(payload)
+  }
+
   getHello() {
     return 'Hello World!'
   }
