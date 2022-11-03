38ea57c6f4c8a7f13c068828c4659ca0708f28c3
Igor <makowskiigor@gmail.com>
2022-11-03 14:46:56 +0100

    feat(auth): create controller

---
+++ apps/auth/src/auth.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Controller,
+  ForbiddenException,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common'
+import { Response } from 'express'
+
+import { SpotifyAuthGuard } from './guards/spotify.guard'
+import { AuthService } from './auth.service'
+import { SpotifyAuthRequest, SpotifyAuth } from './dtos'
+
+@Controller('auth/spotify')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Get('login')
+  @UseGuards(SpotifyAuthGuard)
+  login() {
+    return
+  }
+
+  @Get('callback')
+  @UseGuards(SpotifyAuthGuard)
+  @HttpCode(HttpStatus.CREATED)
+  async callback(
+    @Req() request: SpotifyAuthRequest,
+    @Res() response: Response
+  ): Promise<Response<unknown, SpotifyAuth>> {
+    const { user, authInfo } = request
+
+    console.log(user, authInfo)
+    if (!user) throw new ForbiddenException('User not found')
+
+    request.user = undefined
+
+    const jwt = this.authService.login(user)
+
+    response.set('Authorization', `Bearer ${jwt}`)
+
+    return response.json({ authInfo, user }) as Response<unknown, SpotifyAuth>
+  }
+}
--- apps/auth/src/guards/spotify.guard.ts
+++ apps/auth/src/guards/spotify.guard.ts
@@ -2,4 +2,4 @@ import { Injectable } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 
 @Injectable()
-export class SpotifyOauthGuard extends AuthGuard('spotify') {}
+export class SpotifyAuthGuard extends AuthGuard('spotify') {}
