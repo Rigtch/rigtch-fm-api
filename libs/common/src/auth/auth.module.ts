24b464d8903d097456ad74368a74b806681112a9
Igor <makowskiigor@gmail.com>
2022-11-06 20:19:20 +0100

    fix: jest error with enum

--- libs/common/src/auth/auth.module.ts
+++ libs/common/src/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 
-import { RmqModule, Services } from '..'
+import { RmqModule } from '..'
+import { Services } from '../services'
 
 @Module({
   imports: [RmqModule.register({ name: Services.AUTH })],
