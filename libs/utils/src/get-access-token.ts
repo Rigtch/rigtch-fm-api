0d2f1a3a27c0752572bd48ac27091f7834175783
Igor <makowskiigor@gmail.com>
2022-11-06 21:36:39 +0100

    refactor(@lib/utils): remove `getAccessToken` function

--- libs/utils/src/get-access-token.ts
+++
@@ -1,9 +0,0 @@
-import { Request } from 'express'
-
-export function getAccessToken(request: Request) {
-  const accessToken = request.headers?.authorization?.slice(7)
-
-  if (!accessToken) return
-
-  return accessToken
-}
