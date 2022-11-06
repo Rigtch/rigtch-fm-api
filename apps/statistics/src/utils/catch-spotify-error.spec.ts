dc412db9b447952703effa9486681469f3d68f5f
Igor <makowskiigor@gmail.com>
2022-11-06 20:18:10 +0100

    feat(statistics/utils): create `catchSpotifyError` function

---
+++ apps/statistics/src/utils/catch-spotify-error.spec.ts
@@ -0,0 +1,37 @@
+import {
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common'
+
+import { catchSpotifyError } from './catch-spotify-error'
+
+describe('catchSpotifyError', () => {
+  it('should throw UnauthorizedException', () => {
+    expect(() =>
+      catchSpotifyError({
+        response: {
+          data: {
+            error: {
+              status: 401,
+              message: 'Unauthorized',
+            },
+          },
+        },
+      })
+    ).toThrowError(UnauthorizedException)
+  })
+  it('should throw InternalServerErrorException', () => {
+    expect(() =>
+      catchSpotifyError({
+        response: {
+          data: {
+            error: {
+              status: 500,
+              message: 'Internal Server Error',
+            },
+          },
+        },
+      })
+    ).toThrowError(InternalServerErrorException)
+  })
+})
