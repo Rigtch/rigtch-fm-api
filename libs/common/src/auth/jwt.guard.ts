d52c2781048fc40ce19949c87466e9a39509343c
Igor <makowskiigor@gmail.com>
2022-11-06 20:17:32 +0100

    fix(libs/common): auth guard canActivate method

--- libs/common/src/auth/jwt.guard.ts
+++ libs/common/src/auth/jwt.guard.ts
@@ -1,61 +1,36 @@
+import { GqlExecutionContext } from '@nestjs/graphql'
 import {
   CanActivate,
   ExecutionContext,
-  Inject,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common'
-import { ClientProxy } from '@nestjs/microservices'
-import { catchError, Observable, tap } from 'rxjs'
-
-import { Services } from '..'
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
-  constructor(
-    @Inject(Services.AUTH) private readonly authClient: ClientProxy
-  ) {}
-
-  canActivate(
-    context: ExecutionContext
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    const authentication = this.getAuthentication(context)
-
-    return this.authClient
-      .send('validate_user', {
-        Authentication: authentication,
-      })
-      .pipe(
-        tap(response => {
-          this.addUser(response, context)
-        }),
-        catchError(() => {
-          throw new UnauthorizedException()
-        })
-      )
+  canActivate(context: ExecutionContext): boolean {
+    return !!this.getAuthentication(context)
   }
 
   private getAuthentication(context: ExecutionContext): string {
     let authentication: string
+
     if (context.getType() === 'rpc') {
-      authentication = context.switchToRpc().getData().Authentication
+      authentication = context.switchToRpc().getData().authorization
     } else if (context.getType() === 'http') {
       authentication = context.switchToHttp().getRequest()
-        .cookies?.Authentication
+        .headers?.authorization
+    } else {
+      const newContext = GqlExecutionContext.create(context)
+
+      authentication = newContext.getContext().req.headers?.authorization
     }
+
     if (!authentication) {
       throw new UnauthorizedException(
         'No value was provided for Authentication'
       )
     }
     return authentication
-  }
-
-  private addUser(user: unknown, context: ExecutionContext) {
-    if (context.getType() === 'rpc') {
-      context.switchToRpc().getData().user = user
-    } else if (context.getType() === 'http') {
-      context.switchToHttp().getRequest().user = user
-    }
   }
 }
