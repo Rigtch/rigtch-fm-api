9c1d728413629b0812c26a83821654aebcb61054
Igor <makowskiigor@gmail.com>
2022-11-06 20:19:03 +0100

    refactor(statistics.service): use `rxjs` observable instead of transforming it into promise

--- apps/statistics/src/statistics.resolver.ts
+++ apps/statistics/src/statistics.resolver.ts
@@ -1,31 +1,38 @@
-// import { UseGuards } from '@nestjs/common'
+import { UseGuards } from '@nestjs/common'
 import { Context, Query, Resolver } from '@nestjs/graphql'
+import { firstValueFrom } from 'rxjs'
 
 import { Track } from './dtos'
 import { StatisticsService } from './statistics.service'
 
 import { getAccessToken } from '@lib/utils'
-// import { JwtAuthGuard } from '@lib/common'
+import { JwtAuthGuard } from '@lib/common'
 
 @Resolver()
 export class StatisticsResolver {
   constructor(private readonly statisticsService: StatisticsService) {}
 
-  // @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard)
   @Query(() => [Track], { name: 'lastTracks' })
   async getLastTracks(@Context() { req: request }) {
-    return this.statisticsService.getlastTracks(getAccessToken(request))
+    return await firstValueFrom(
+      this.statisticsService.getlastTracks(getAccessToken(request))
+    )
   }
 
-  // @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard)
   @Query(() => [Track], { name: 'topTracks' })
   async getTopTracks(@Context() { req: request }) {
-    return this.statisticsService.getTopTracks(getAccessToken(request))
+    return await firstValueFrom(
+      this.statisticsService.getTopTracks(getAccessToken(request))
+    )
   }
 
-  // @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard)
   @Query(() => [Track], { name: 'topArtists' })
   async getTopArtists(@Context() { req: request }) {
-    return this.statisticsService.getTopArtists(getAccessToken(request))
+    return await firstValueFrom(
+      this.statisticsService.getTopArtists(getAccessToken(request))
+    )
   }
 }
