68906aee7f05998997aa55a1ab1ea7824c10b4a4
Igor <makowskiigor@gmail.com>
2022-11-06 21:50:48 +0100

    feat(auth): implement refreshing token

--- apps/auth/src/auth.service.ts
+++ apps/auth/src/auth.service.ts
@@ -1,10 +1,22 @@
+import { HttpService } from '@nestjs/axios'
 import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { JwtService } from '@nestjs/jwt'
 import { Profile } from 'passport-spotify'
+import { catchError, map, Observable } from 'rxjs'
+
+import { Environment } from './config'
+import { RefreshResponse, SpotifyTokenResponse } from './dtos'
+
+import { catchSpotifyError } from '@lib/utils'
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService
+  ) {}
 
   login({ id, username }: Profile) {
     const payload = {
@@ -13,6 +25,38 @@ export class AuthService {
     }
 
     return this.jwtService.sign(payload)
+  }
+
+  refresh(refreshToken: string): Observable<RefreshResponse> {
+    const cliendId = this.configService.get(Environment.SPOTIFY_CLIENT_ID)
+    const clientSecret = this.configService.get(
+      Environment.SPOTIFY_CLIENT_SECRET
+    )
+    const bufferedCredentials = Buffer.from(
+      `${cliendId}:${clientSecret}`
+    ).toString('base64')
+    const parameters = new URLSearchParams()
+
+    parameters.append('grant_type', 'refresh_token')
+    parameters.append('refresh_token', refreshToken)
+
+    return this.httpService
+      .post<SpotifyTokenResponse>('/api/token', parameters, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Authorization: `Basic ${bufferedCredentials}`,
+          json: true,
+        },
+      })
+      .pipe(
+        map(response => response.data),
+        map(({ access_token, refresh_token, expires_in }) => ({
+          accessToken: access_token,
+          refreshToken: refresh_token,
+          expiresIn: expires_in,
+        })),
+        catchError(catchSpotifyError)
+      )
   }
 
   getHello() {
