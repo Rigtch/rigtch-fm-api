51f3bcf666fe4e5e61f9e292e29aa5865e631719
Igor <makowskiigor@gmail.com>
2022-11-08 22:50:16 +0100

    feat(statistics): connect microservice

--- apps/statistics/src/main.ts
+++ apps/statistics/src/main.ts
@@ -1,13 +1,20 @@
 import { NestFactory } from '@nestjs/core'
 import { ConfigService } from '@nestjs/config'
+import { RmqOptions } from '@nestjs/microservices'
 
 import { StatisticsModule } from './statistics.module'
 import { Environment } from './config'
 
+import { RmqService, Service } from '@lib/common'
+
 const app = await NestFactory.create(StatisticsModule)
 const configService = app.get(ConfigService)
+const rmqService = app.get(RmqService)
 
 app.enableCors()
+app.connectMicroservice<RmqOptions>(
+  rmqService.getOptions(Service.STATISTICS, true)
+)
 
 await app.startAllMicroservices()
 await app.listen(+configService.get(Environment.PORT) || 4000)
