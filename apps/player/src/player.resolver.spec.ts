1ea9e896b01a82c88dd218ce7f2410f7510378ea
Igor <makowskiigor@gmail.com>
2022-11-10 21:22:28 +0100

    feat(player.resolver): create `pausePlayer` method

--- apps/player/src/player.resolver.spec.ts
+++ apps/player/src/player.resolver.spec.ts
@@ -1,19 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { of } from 'rxjs'
 
+import { formattedDevicesMock } from './../../../libs/common/src/spotify/mocks/formatted-device.mock'
 import { PlayerResolver } from './player.resolver'
+import { PlayerService } from './player.service'
 
 describe('PlayerResolver', () => {
-  let resolver: PlayerResolver
+  let playerResolver: PlayerResolver
+  let playerService: PlayerService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PlayerResolver],
+      providers: [
+        PlayerResolver,
+        {
+          provide: PlayerService,
+          useValue: {
+            avaibleDevices: jest.fn(),
+            pausePlayer: jest.fn(),
+          },
+        },
+      ],
     }).compile()
 
-    resolver = module.get<PlayerResolver>(PlayerResolver)
+    playerResolver = module.get<PlayerResolver>(PlayerResolver)
+    playerService = module.get<PlayerService>(PlayerService)
   })
 
   it('should be defined', () => {
-    expect(resolver).toBeDefined()
+    expect(playerResolver).toBeDefined()
+  })
+
+  it('should get available devices', async () => {
+    playerService.avaibleDevices = jest
+      .fn()
+      .mockReturnValue(of(formattedDevicesMock))
+
+    expect(await playerResolver.getAvaibleDevices('awd')).toEqual(
+      formattedDevicesMock
+    )
+  })
+
+  describe('pausePlayer', () => {
+    it('should pause player', async () => {
+      playerService.pausePlayer = jest
+        .fn()
+        .mockReturnValue(of({ success: true }))
+
+      expect(await playerResolver.pausePlayer('awd')).toEqual({ success: true })
+    })
   })
 })
--- apps/player/src/player.resolver.ts
+++ apps/player/src/player.resolver.ts
@@ -1,11 +1,11 @@
 import { firstValueFrom } from 'rxjs'
-import { Query, Resolver } from '@nestjs/graphql'
+import { Args, Query, Resolver } from '@nestjs/graphql'
 import { UseGuards } from '@nestjs/common'
 
 import { PlayerService } from './player.service'
 import { Device } from './dtos'
 
-import { AccessToken, JwtAuthGuard } from '@lib/common'
+import { AccessToken, JwtAuthGuard, Success } from '@lib/common'
 
 @Resolver()
 export class PlayerResolver {
@@ -15,5 +15,17 @@ export class PlayerResolver {
   @UseGuards(JwtAuthGuard)
   async getAvaibleDevices(@AccessToken() accessToken: string) {
     return await firstValueFrom(this.playerService.avaibleDevices(accessToken))
+  }
+
+  @Query(() => Success, { name: 'pausePlayer' })
+  @UseGuards(JwtAuthGuard)
+  async pausePlayer(
+    @AccessToken() accessToken: string,
+    @Args('afterTime', { nullable: true }) afterTime?: number,
+    @Args('deviceId', { nullable: true }) deviceId?: string
+  ) {
+    return await firstValueFrom(
+      this.playerService.pausePlayer(accessToken, afterTime, deviceId)
+    )
   }
 }
