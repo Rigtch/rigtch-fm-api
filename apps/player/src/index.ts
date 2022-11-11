893185eefe8770eaa89f68822c6fb8a32285a9b4
Igor <makowskiigor@gmail.com>
2022-11-11 06:19:27 +0100

    chore(player): export missing files

--- apps/player/src/index.ts
+++ apps/player/src/index.ts
@@ -1,3 +1,5 @@
 export { Environment as PlayerEnvironment } from './config'
+export * from './messages'
 export * from './player.module'
 export * from './player.service'
+export * from './player.resolver'
