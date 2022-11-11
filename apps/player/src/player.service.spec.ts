faedf73e9a11303390a3de86b42720aa5688d3e5
Igor <makowskiigor@gmail.com>
2022-11-11 05:56:02 +0100

    feat(player.service): create `resumePlayer` and `currentPlaybackState

--- apps/player/src/player.service.spec.ts
+++ apps/player/src/player.service.spec.ts
@@ -1,12 +1,15 @@
 import { HttpService } from '@nestjs/axios'
 import { TestingModule, Test } from '@nestjs/testing'
-import { of, firstValueFrom } from 'rxjs'
+import { of, firstValueFrom, throwError, catchError } from 'rxjs'
+import { ForbiddenException } from '@nestjs/common'
 
 import { PlayerService } from './player.service'
 
 import {
   formattedDevicesMock,
+  formattedPlaybackStateMock,
   spotifyDevicesMock,
+  spotifyPlaybackStateMock,
   SpotifyService,
 } from '@lib/common'
 
@@ -51,23 +54,71 @@ describe('PlayerService', () => {
     )
   })
 
+  it('currentPlaybackState', async () => {
+    httpService.get = jest.fn().mockReturnValue(
+      of({
+        data: spotifyPlaybackStateMock,
+      })
+    )
+
+    expect(
+      await firstValueFrom(playerService.currentPlaybackState('awd'))
+    ).toEqual(formattedPlaybackStateMock)
+  })
+
   describe('pausePlayer', () => {
     it('should pause player', async () => {
-      httpService.get = jest.fn().mockReturnValue(
-        of({
-          data: {
-            devices: spotifyDevicesMock,
+      expect(await firstValueFrom(playerService.pausePlayer('awd'))).toEqual({
+        success: true,
+      })
+    })
+
+    it('should throw Forbidden expception because No device is currently playing', async () => {
+      httpService.put = jest.fn().mockReturnValue(
+        throwError(() => ({
+          response: {
+            data: {
+              error: {
+                status: 403,
+              },
+            },
           },
-        })
+        }))
       )
 
       expect(
         await firstValueFrom(
-          await firstValueFrom(playerService.pausePlayer('awd'))
+          playerService.pausePlayer('awd').pipe(catchError(error => [error]))
         )
-      ).toEqual({
+      ).toBeInstanceOf(ForbiddenException)
+    })
+  })
+
+  describe('resumePlayer', () => {
+    it('should resume player', async () => {
+      expect(await firstValueFrom(playerService.resumePlayer('awd'))).toEqual({
         success: true,
       })
+    })
+
+    it('should throw Forbidden expception because No device is currently playing', async () => {
+      httpService.put = jest.fn().mockReturnValue(
+        throwError(() => ({
+          response: {
+            data: {
+              error: {
+                status: 403,
+              },
+            },
+          },
+        }))
+      )
+
+      expect(
+        await firstValueFrom(
+          playerService.resumePlayer('awd').pipe(catchError(error => [error]))
+        )
+      ).toBeInstanceOf(ForbiddenException)
     })
   })
 })
