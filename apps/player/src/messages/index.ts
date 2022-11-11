e28da30b830b0ef40dc20ba2efa04a18fbfc78ff
Igor <makowskiigor@gmail.com>
2022-11-11 05:55:06 +0100

    feat(player): create messages enum

---
+++ apps/player/src/messages/index.ts
@@ -0,0 +1,5 @@
+export enum PlayerMessage {
+  NO_AVAIBLE_DEVICES = 'No device is avaible',
+  NO_PLAYING_DEVICE = 'No device is currently playing',
+  DEVICE_ALREADY_PLAYING = 'Device is already playing',
+}
