88f0305cdda72a5f57f2b85329bd74caecabeb26
Igor <makowskiigor@gmail.com>
2022-12-10 16:14:15 +0100

    refactor(auth): adjust types for new structure

--- apps/auth/src/dtos/spotify-auth.dto.ts
+++
@@ -1,8 +0,0 @@
-import { Profile } from 'passport-spotify'
-
-import { AuthInfo } from '.'
-
-export abstract class SpotifyAuth {
-  user: Profile
-  authInfo: AuthInfo
-}
--- apps/auth/src/types/spotify-auth-response.ts
+++ apps/auth/src/types/spotify-auth-response.ts
@@ -1,5 +1,7 @@
 import { Request } from 'express'
 
-import { SpotifyAuth } from '../dtos'
-
-export type SpotifyAuthRequest = SpotifyAuth & Request
+export type SpotifyAuthRequest = Request & {
+  query: {
+    code: string
+  }
+}
