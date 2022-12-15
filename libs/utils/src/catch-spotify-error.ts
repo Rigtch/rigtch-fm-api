b5e07981986487ce890bc91fdbcc0771ca8f3063
Igor <makowskiigor@gmail.com>
2022-12-15 13:08:43 +0100

    fix(catch-spotify-error): catch forbidden `not registered in developer dashboard`

--- libs/utils/src/catch-spotify-error.ts
+++ libs/utils/src/catch-spotify-error.ts
@@ -1,19 +1,23 @@
 import {
+  ForbiddenException,
   InternalServerErrorException,
   UnauthorizedException,
 } from '@nestjs/common'
 
-export const catchSpotifyError = ({
-  response: {
-    data: { error },
-  },
-}) => {
-  if (error.status === 401) throw new UnauthorizedException(error.message)
-  if (error === 'invalid_grant')
+export const catchSpotifyError = ({ response: { data, status } }) => {
+  if (status === 401) throw new UnauthorizedException(data.error.message)
+  if (data.error === 'invalid_grant')
     throw new UnauthorizedException('Invalid token')
+  if (
+    status === 403 &&
+    data === 'User not registered in the Developer Dashboard'
+  )
+    throw new ForbiddenException(
+      'User not registered in the Developer Dashboard'
+    )
 
   throw new InternalServerErrorException(
     'Something went wrong with fetching data from spotify API',
-    error
+    data?.error
   )
 }
