4e233f865c86f2f17de7d4a6e14320d8e99fb490
Mnigos <makowskiigor@gmail.com>
2023-07-20 14:45:50 +0200

    feat(main): setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,41 @@
 import { NestFactory } from '@nestjs/core'
 import { ConfigService } from '@nestjs/config'
 import * as cookieParser from 'cookie-parser'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
-import { AppModule } from './modules/app'
 import { Environment } from './config'
+
+import { AppModule } from '@modules/app'
+import { AuthenticationType } from '@modules/auth/enums'
+import { BEARER } from '@modules/auth/constants'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const configService = app.get(ConfigService)
+
+  const documentConfig = new DocumentBuilder()
+    .setTitle('Rigtch Music API')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: BEARER,
+        bearerFormat: 'JWT',
+      },
+      AuthenticationType.ACCESS_TOKEN
+    )
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: BEARER,
+        bearerFormat: 'JWT',
+      },
+      AuthenticationType.REFRESH_TOKEN
+    )
+    .build()
+
+  const document = SwaggerModule.createDocument(app, documentConfig)
+
+  SwaggerModule.setup('api', app, document)
 
   app.enableCors({
     origin: configService.get(Environment.CLIENT_CALLBACK_URL),
