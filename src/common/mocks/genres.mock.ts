f08916ca58120a3dc07b92ef8ce020376f84fa74
Mnigos <makowskiigor@gmail.com>
2023-07-23 13:20:11 +0200

    style(common/mocks/genres): add type

--- src/common/mocks/genres.mock.ts
+++ src/common/mocks/genres.mock.ts
@@ -1,9 +1,11 @@
+import { Genres } from '../dtos'
+
 export const topGenresArrayMock = [
   "black 'n' roll",
   'black metal',
   'blackened crust',
 ]
 
-export const topGenresMock = {
+export const topGenresMock: Genres = {
   genres: topGenresArrayMock,
 }
