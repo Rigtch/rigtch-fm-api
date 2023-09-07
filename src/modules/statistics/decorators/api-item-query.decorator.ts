4a9e3a94d85c208c3df1a387b878ef595c0b88d8
Mnigos <makowskiigor@gmail.com>
2023-09-07 15:55:57 +0200

    refactor(modules/statistics/decorators): adjust `ApiItemQuery` dtos for `Cursors`

--- src/modules/statistics/decorators/api-item-query.decorator.ts
+++ src/modules/statistics/decorators/api-item-query.decorator.ts
@@ -3,11 +3,22 @@ import { ApiQuery } from '@nestjs/swagger'
 
 import { TimeRange } from '../enums'
 
-export const ApiItemQuery = (isTopItem = false) =>
+export interface ApiItemQueryOptions {
+  withOffset?: boolean
+  withCursors?: boolean
+}
+
+export const ApiItemQuery = (options?: ApiItemQueryOptions) =>
   applyDecorators(
     ApiQuery({ name: 'limit', type: Number, required: false }),
     ApiQuery({ name: 'timeRange', enum: TimeRange, required: false }),
-    isTopItem
+    options?.withOffset
       ? ApiQuery({ name: 'offset', type: Number, required: false })
+      : () => {},
+    options?.withCursors
+      ? ApiQuery({ name: 'after', required: false })
+      : () => {},
+    options?.withCursors
+      ? ApiQuery({ name: 'before', required: false })
       : () => {}
   )
