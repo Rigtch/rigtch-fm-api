a811833909fe8e53b1fea6c1a312d12173817a5d
Mnigos <makowskiigor@gmail.com>
2023-09-13 15:08:41 +0200

    chore: remove dist

--- dist/common/types/utils/require-only-one.d.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-
-//# sourceMappingURL=require-only-one.d.js.map
--- dist/modules/auth/types/redirect-response.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-
-//# sourceMappingURL=redirect-response.js.map
