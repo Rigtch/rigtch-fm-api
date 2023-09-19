b82bd18a385a4fa8ce5b06c0aa06d137c791981d
Mnigos <makowskiigor@gmail.com>
2023-09-19 13:33:50 +0200

    refactor(config): move `environment` files to `/config/environment`

---
+++ src/config/environment/environment.schema.ts
@@ -0,0 +1,17 @@
+import Joi from 'joi'
+
+export const environmentSchema = Joi.object({
+  PORT: Joi.number().default(4000),
+  SPOTIFY_CLIENT_ID: Joi.string().required(),
+  SPOTIFY_CLIENT_SECRET: Joi.string().required(),
+  SPOTIFY_CALLBACK_URL: Joi.string().required(),
+  SPOTIFY_BASE_URL: Joi.string().required(),
+  SPOTIFY_ACCOUNTS_URL: Joi.string().required(),
+  CLIENT_CALLBACK_URL: Joi.string().required(),
+  JWT_SECRET: Joi.string().required(),
+  DATABASE_HOST: Joi.string().required(),
+  DATABASE_PORT: Joi.number().required(),
+  DATABASE_USERNAME: Joi.string().required(),
+  DATABASE_PASSWORD: Joi.string().required(),
+  DATABASE_NAME: Joi.string().required(),
+})
