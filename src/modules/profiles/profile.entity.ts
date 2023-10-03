f34993f9c46ecb53179687288b0dbc1b6bec631e
Mnigos <makowskiigor@gmail.com>
2023-10-03 10:08:01 +0200

    feat(modules): create `profile` module

---
+++ src/modules/profiles/profile.entity.ts
@@ -0,0 +1,59 @@
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryColumn,
+  Relation,
+} from 'typeorm'
+import { ApiProperty } from '@nestjs/swagger'
+
+import { Image } from '@modules/images'
+import { FormattedProfile } from '@common/types/spotify'
+
+@Entity()
+export class Profile implements FormattedProfile {
+  @PrimaryColumn()
+  @ApiProperty()
+  id: string
+
+  @Column()
+  @ApiProperty()
+  displayName: string
+
+  @ManyToMany('Image', 'profiles', {
+    cascade: true,
+    nullable: true,
+  })
+  @JoinTable()
+  @ApiProperty({ type: Image, isArray: true })
+  images: Relation<Image>[]
+
+  @Column('int')
+  @ApiProperty({ type: Number })
+  followers: number
+
+  @Column({ nullable: true })
+  @ApiProperty({ nullable: true })
+  country?: string
+
+  @Column({ nullable: true })
+  @ApiProperty({ nullable: true })
+  email?: string
+
+  @Column()
+  @ApiProperty()
+  href: string
+
+  @Column({ nullable: true })
+  @ApiProperty({ nullable: true })
+  product?: string
+
+  @Column()
+  @ApiProperty()
+  type: string
+
+  @Column()
+  @ApiProperty()
+  uri: string
+}
