039b8292cafdb2c50ee18fdd417a4d9ea92c9f88
Mnigos <makowskiigor@gmail.com>
2023-10-17 12:53:03 +0200

    feat(modules/profiles): implement `ProfileService` with `create` method

--- src/modules/profiles/index.ts
+++ src/modules/profiles/index.ts
@@ -1,3 +1,4 @@
 export * from './profile.entity'
 export * from './profiles.repository'
 export * from './profiles.module'
+export * from './profiles.service'
--- src/modules/profiles/profiles.module.ts
+++ src/modules/profiles/profiles.module.ts
@@ -3,10 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { Profile } from './profile.entity'
 import { ProfilesRepository } from './profiles.repository'
+import { ProfilesService } from './profiles.service'
+
+import { ImagesModule } from '@modules/images'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Profile])],
-  providers: [ProfilesRepository],
-  exports: [ProfilesRepository],
+  imports: [TypeOrmModule.forFeature([Profile]), ImagesModule],
+  providers: [ProfilesRepository, ProfilesService],
+  exports: [ProfilesRepository, ProfilesService],
 })
 export class ProfilesModule {}
