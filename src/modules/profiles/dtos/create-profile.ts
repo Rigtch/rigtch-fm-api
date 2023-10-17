5070eeb695f94f4f4d914352c9e717505794540e
Mnigos <makowskiigor@gmail.com>
2023-10-17 12:52:14 +0200

    feat(modules/profiles/dtos): implement `CreateProfile`

---
+++ src/modules/profiles/dtos/create-profile.ts
@@ -0,0 +1,37 @@
+import { ApiProperty } from '@nestjs/swagger'
+
+import { Profile } from '../profile.entity'
+
+import { SpotifyImage } from '@common/types/spotify'
+
+export abstract class CreateProfile implements Omit<Profile, 'images'> {
+  @ApiProperty()
+  id: string
+
+  @ApiProperty()
+  displayName: string
+
+  @ApiProperty()
+  images: SpotifyImage[]
+
+  @ApiProperty({ type: Number })
+  followers: number
+
+  @ApiProperty({ nullable: true })
+  country?: string
+
+  @ApiProperty({ nullable: true })
+  email?: string
+
+  @ApiProperty()
+  href: string
+
+  @ApiProperty({ nullable: true })
+  product?: string
+
+  @ApiProperty()
+  type: string
+
+  @ApiProperty()
+  uri: string
+}
