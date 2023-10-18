6db32b94d70c4bd888ccf52a937883476895c98a
Mnigos <makowskiigor@gmail.com>
2023-10-18 20:42:40 +0200

    refactor(modules/auth/decorators/token): create `getToken` function`

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,4 @@
 export * from './get-most-frequent-items'
 export * from './catch-spotify-error'
-export * from './get-parameter-decorator-factory'
 export * from './apply-authorization-header'
 export * from './axios-response-mock-factory'
