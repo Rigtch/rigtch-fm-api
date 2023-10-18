92231045389926a87e4d1828852ed1d99bc3cf03
Mnigos <makowskiigor@gmail.com>
2023-10-18 22:06:43 +0200

    feat(modules/users): implement `controller`

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { User } from './user.entity'
 import { UsersRepository } from './users.repository'
+import { UsersController } from './users.controller'
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   providers: [UsersRepository],
+  controllers: [UsersController],
   exports: [UsersRepository],
 })
 export class UsersModule {}
