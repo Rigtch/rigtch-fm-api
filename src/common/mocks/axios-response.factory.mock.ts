cdc9e73259eeecf475294049708e8fda88750928
Mnigos <makowskiigor@gmail.com>
2023-12-09 01:23:26 +0100

    refactor(common/utils/catch-spotify-error): add typings

--- src/common/mocks/axios-response.factory.mock.ts
+++ src/common/mocks/axios-response.factory.mock.ts
@@ -1,10 +1,11 @@
 import { AxiosResponse, InternalAxiosRequestConfig } from 'axios'
 
 export const axiosResponseMockFactory = <TData = unknown>(
-  data: TData
+  data: TData,
+  status = 200
 ): AxiosResponse<TData> => ({
   data,
-  status: 200,
+  status,
   statusText: 'OK',
   headers: {},
   config: {} as InternalAxiosRequestConfig,
--- src/common/utils/catch-spotify-error.spec.ts
+++ src/common/utils/catch-spotify-error.spec.ts
@@ -1,52 +1,53 @@
 import { test, describe, expect } from 'vitest'
-import {
-  InternalServerErrorException,
-  UnauthorizedException,
-} from '@nestjs/common'
+import { BadGatewayException, UnauthorizedException } from '@nestjs/common'
 
 import { catchSpotifyError } from './catch-spotify-error'
+
+import { axiosResponseMockFactory } from '@common/mocks'
 
 describe('catchSpotifyError', () => {
   test('should throw UnauthorizedException', () => {
     expect(() =>
-      catchSpotifyError({
-        response: {
-          data: {
+      catchSpotifyError(
+        axiosResponseMockFactory(
+          {
             error: {
               message: 'Unauthorized',
+              status: 401,
             },
           },
-          status: 401,
-        },
-      })
+          401
+        )
+      )
     ).toThrowError(UnauthorizedException)
   })
 
   test('should throw UnauthorizedException as invalid grant', () => {
     expect(() =>
-      catchSpotifyError({
-        response: {
-          data: {
-            error: 'invalid_grant',
+      catchSpotifyError(
+        axiosResponseMockFactory(
+          {
+            error: {
+              message: 'invalid_grant',
+              status: 401,
+            },
           },
-          status: 401,
-        },
-      })
+          401
+        )
+      )
     ).toThrowError(UnauthorizedException)
   })
 
   test('should throw InternalServerErrorException', () => {
     expect(() =>
-      catchSpotifyError({
-        response: {
-          data: {
-            error: {
-              message: 'Internal Server Error',
-            },
+      catchSpotifyError(
+        axiosResponseMockFactory({
+          error: {
+            message: 'Bad Gateway',
+            status: 500,
           },
-          status: 500,
-        },
-      })
-    ).toThrowError(InternalServerErrorException)
+        })
+      )
+    ).toThrowError(BadGatewayException)
   })
 })
