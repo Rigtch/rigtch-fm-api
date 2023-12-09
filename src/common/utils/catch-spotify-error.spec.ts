c4d39f96b710cfe8b833487c64809d0400480e57
Mnigos <makowskiigor@gmail.com>
2023-12-09 01:46:39 +0100

    test(common/utils/catch-spotify-error): fix coverage

--- src/common/utils/catch-spotify-error.spec.ts
+++ src/common/utils/catch-spotify-error.spec.ts
@@ -1,53 +1,78 @@
 import { test, describe, expect } from 'vitest'
 import { BadGatewayException, UnauthorizedException } from '@nestjs/common'
 
-import { catchSpotifyError } from './catch-spotify-error'
+import {
+  SPOTIFY_DEFAULT_ERROR_MESSAGE,
+  SpotifyAuthError,
+  catchSpotifyError,
+} from './catch-spotify-error'
 
 import { axiosResponseMockFactory } from '@common/mocks'
 
 describe('catchSpotifyError', () => {
   test('should throw UnauthorizedException', () => {
+    const message = 'Unauthorized'
+
     expect(() =>
       catchSpotifyError(
         axiosResponseMockFactory(
           {
             error: {
-              message: 'Unauthorized',
+              message,
               status: 401,
             },
           },
           401
         )
       )
-    ).toThrowError(UnauthorizedException)
+    ).toThrowError(new UnauthorizedException(message))
   })
 
   test('should throw UnauthorizedException as invalid grant', () => {
+    const message = 'Invalid token'
+
     expect(() =>
       catchSpotifyError(
         axiosResponseMockFactory(
           {
-            error: {
-              message: 'invalid_grant',
-              status: 401,
-            },
+            error: 'invalid_grant',
+            error_description: message,
           },
           401
         )
       )
-    ).toThrowError(UnauthorizedException)
+    ).toThrowError(new UnauthorizedException(message))
   })
 
-  test('should throw InternalServerErrorException', () => {
+  test('should throw BadGatewayException', () => {
+    const message = 'Bad Gateway'
+
     expect(() =>
       catchSpotifyError(
         axiosResponseMockFactory({
           error: {
-            message: 'Bad Gateway',
-            status: 500,
+            message,
+            status: 502,
           },
         })
       )
-    ).toThrowError(BadGatewayException)
+    ).toThrowError(
+      new BadGatewayException(SPOTIFY_DEFAULT_ERROR_MESSAGE + message)
+    )
+  })
+
+  test('should throw BadGatewayException', () => {
+    const message = 'Bad Gateway'
+
+    expect(() =>
+      catchSpotifyError(
+        axiosResponseMockFactory<SpotifyAuthError>({
+          error: message,
+          error_description: message,
+        })
+      )
+    ).toThrowError(
+      new BadGatewayException(SPOTIFY_DEFAULT_ERROR_MESSAGE + message)
+    )
   })
 })
