55e54818f1a6f199e15df0eb5100dc9f41752c35
Mnigos <makowskiigor@gmail.com>
2024-02-07 19:28:47 +0100

    refactor(modules/users): remove service`

--- src/modules/profiles/profiles.module.ts
+++ src/modules/profiles/profiles.module.ts
@@ -3,13 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { Profile } from './profile.entity'
 import { ProfilesRepository } from './profiles.repository'
-import { ProfilesService } from './profiles.service'
 
 import { ImagesModule } from '@modules/images'
 
 @Module({
   imports: [TypeOrmModule.forFeature([Profile]), ImagesModule],
-  providers: [ProfilesRepository, ProfilesService],
-  exports: [ProfilesRepository, ProfilesService],
+  providers: [ProfilesRepository],
+  exports: [ProfilesRepository],
 })
 export class ProfilesModule {}
