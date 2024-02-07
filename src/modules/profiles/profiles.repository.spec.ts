0bc326e98bf483595a6ad8acc592538a618f867c
Mnigos <makowskiigor@gmail.com>
2024-02-07 19:29:26 +0100

    refactor(modules/profiles/repository): use `findOrCreateImages` method from `ImagesRepository`

--- src/modules/profiles/profiles.repository.spec.ts
+++ src/modules/profiles/profiles.repository.spec.ts
@@ -3,10 +3,12 @@ import { Test, TestingModule } from '@nestjs/testing'
 
 import { ProfilesRepository } from './profiles.repository'
 
-import { profileMock } from '@common/mocks'
+import { imagesMock, profileMock } from '@common/mocks'
+import { ImagesRepository } from '@modules/images'
 
 describe('ProfilesRepository', () => {
   let profilesRepository: ProfilesRepository
+  let imagesRepository: ImagesRepository
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -18,10 +20,17 @@ describe('ProfilesRepository', () => {
             createEntityManager: vi.fn(),
           },
         },
+        {
+          provide: ImagesRepository,
+          useValue: {
+            findOrCreateImages: vi.fn(),
+          },
+        },
       ],
     }).compile()
 
-    profilesRepository = module.get<ProfilesRepository>(ProfilesRepository)
+    profilesRepository = module.get(ProfilesRepository)
+    imagesRepository = module.get(ImagesRepository)
   })
 
   test('should be defined', () => {
@@ -35,11 +44,15 @@ describe('ProfilesRepository', () => {
     const saveSpy = vi
       .spyOn(profilesRepository, 'save')
       .mockResolvedValue(profileMock)
+    const findOrCreateImagesSpy = vi
+      .spyOn(imagesRepository, 'findOrCreateImages')
+      .mockResolvedValue(imagesMock)
 
     expect(await profilesRepository.createProfile(profileMock)).toEqual(
       profileMock
     )
     expect(createSpy).toHaveBeenCalledWith(profileMock)
     expect(saveSpy).toHaveBeenCalledWith(profileMock)
+    expect(findOrCreateImagesSpy).toHaveBeenCalledWith(profileMock.images)
   })
 })
