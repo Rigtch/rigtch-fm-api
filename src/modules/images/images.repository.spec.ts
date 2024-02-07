ca3790fce130ed959d212465d8a6d33ac4b19516
Mnigos <makowskiigor@gmail.com>
2024-02-07 19:28:05 +0100

    feat(modules/images): add `createOrFindImages` to repository`

--- src/modules/images/images.repository.spec.ts
+++ src/modules/images/images.repository.spec.ts
@@ -3,7 +3,12 @@ import { DataSource } from 'typeorm'
 
 import { ImagesRepository } from './images.repository'
 
-import { imageMock, sdkImageMock } from '@common/mocks'
+import {
+  imageMock,
+  imagesMock,
+  sdkImageMock,
+  sdkImagesMock,
+} from '@common/mocks'
 
 describe('ImagesRepository', () => {
   let imagesRepository: ImagesRepository
@@ -28,6 +33,19 @@ describe('ImagesRepository', () => {
     expect(imagesRepository).toBeDefined()
   })
 
+  test('should find image by url', async () => {
+    const findOneSpy = vi
+      .spyOn(imagesRepository, 'findOne')
+      .mockResolvedValue(imageMock)
+
+    expect(await imagesRepository.findImageByUrl(sdkImageMock.url)).toEqual(
+      imageMock
+    )
+    expect(findOneSpy).toHaveBeenCalledWith({
+      where: { url: sdkImageMock.url },
+    })
+  })
+
   test('should create image', async () => {
     const createSpy = vi
       .spyOn(imagesRepository, 'create')
@@ -39,5 +57,49 @@ describe('ImagesRepository', () => {
     expect(await imagesRepository.createImage(sdkImageMock)).toEqual(imageMock)
     expect(createSpy).toHaveBeenCalledWith(sdkImageMock)
     expect(saveSpy).toHaveBeenCalledWith(imageMock)
+  })
+
+  describe('findOrCreateImage', () => {
+    test('should find image by url', async () => {
+      const findOneSpy = vi
+        .spyOn(imagesRepository, 'findOne')
+        .mockResolvedValue(imageMock)
+      const createImageSpy = vi.spyOn(imagesRepository, 'createImage')
+
+      expect(await imagesRepository.findOrCreateImage(sdkImageMock)).toEqual(
+        imageMock
+      )
+      expect(findOneSpy).toHaveBeenCalledWith({
+        where: { url: sdkImageMock.url },
+      })
+      expect(createImageSpy).not.toHaveBeenCalled()
+    })
+
+    test('should create image', async () => {
+      vi.spyOn(imagesRepository, 'findOne').mockResolvedValue(null)
+      const createImageSpy = vi
+        .spyOn(imagesRepository, 'createImage')
+        .mockResolvedValue(imageMock)
+      const findOneSpy = vi.spyOn(imagesRepository, 'findOne')
+
+      expect(await imagesRepository.findOrCreateImage(sdkImageMock)).toEqual(
+        imageMock
+      )
+      expect(createImageSpy).toHaveBeenCalledWith(sdkImageMock)
+      expect(findOneSpy).toHaveBeenCalledWith({
+        where: { url: sdkImageMock.url },
+      })
+    })
+  })
+
+  test('should find or create images', async () => {
+    const findOrCreateImageSpy = vi
+      .spyOn(imagesRepository, 'findOrCreateImage')
+      .mockResolvedValue(imageMock)
+
+    expect(await imagesRepository.findOrCreateImages(sdkImagesMock)).toEqual(
+      imagesMock
+    )
+    expect(findOrCreateImageSpy).toHaveBeenCalledTimes(sdkImagesMock.length)
   })
 })
