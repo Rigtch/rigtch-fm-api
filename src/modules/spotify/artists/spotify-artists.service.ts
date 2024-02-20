23f11ff2e8b09ae330bd999be2e7e2d86c47290e
Mnigos <makowskiigor@gmail.com>
2024-02-20 12:47:54 +0100

    refactor(modules/spotify/artists/service): make adaption optional

--- src/modules/spotify/artists/spotify-artists.service.ts
+++ src/modules/spotify/artists/spotify-artists.service.ts
@@ -4,6 +4,7 @@ import { SpotifyApi } from '@spotify/web-api-ts-sdk'
 
 import { AdaptersService } from '@common/adapters'
 import { Environment } from '@config/environment'
+import { Artist, SdkArtist } from '@common/types/spotify'
 
 @Injectable()
 export class SpotifyArtistsService {
@@ -14,14 +15,17 @@ export class SpotifyArtistsService {
     private readonly adaptersService: AdaptersService
   ) {}
 
-  async getArtist(id: string) {
+  public getArtist(id: string, adapt: true): Promise<Artist>
+  public getArtist(id: string, adapt: false): Promise<SdkArtist>
+
+  async getArtist(id: string, adapt = true) {
     this.spotifySdk = SpotifyApi.withClientCredentials(
       this.configService.get<string>(Environment.SPOTIFY_CLIENT_ID)!,
       this.configService.get<string>(Environment.SPOTIFY_CLIENT_SECRET)!
     )
 
-    return this.spotifySdk.artists
-      .get(id)
-      .then(data => this.adaptersService.artists.adapt(data))
+    const data = await this.spotifySdk.artists.get(id)
+
+    return adapt ? this.adaptersService.artists.adapt(data) : data
   }
 }
