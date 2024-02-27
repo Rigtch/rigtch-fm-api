1b88e9127c225325019d8aaa269b63ce4d5ecbf1
Mnigos <makowskiigor@gmail.com>
2024-02-27 09:59:55 +0100

    feat(modules/spotify/tracks/service): add `getTracks` method

--- src/modules/spotify/tracks/spotify-tracks.service.ts
+++ src/modules/spotify/tracks/spotify-tracks.service.ts
@@ -1,9 +1,12 @@
 import { ConfigService } from '@nestjs/config'
 import { SpotifyApi } from '@spotify/web-api-ts-sdk'
+import { Injectable } from '@nestjs/common'
 
 import { AdaptersService } from '@common/adapters'
 import { Environment } from '@config/environment'
+import { SdkTrack, Track } from '@common/types/spotify'
 
+@Injectable()
 export class SpotifyTracksService {
   private spotifySdk: SpotifyApi | undefined
 
@@ -12,14 +15,31 @@ export class SpotifyTracksService {
     private readonly adaptersService: AdaptersService
   ) {}
 
-  async getTrack(id: string) {
+  public getTrack(id: string, adapt: false): Promise<SdkTrack>
+  public getTrack(id: string, adapt: true): Promise<Track>
+
+  async getTrack(id: string, adapt = true) {
     this.spotifySdk = SpotifyApi.withClientCredentials(
       this.configService.get<string>(Environment.SPOTIFY_CLIENT_ID)!,
       this.configService.get<string>(Environment.SPOTIFY_CLIENT_SECRET)!
     )
 
-    return this.spotifySdk.tracks
-      .get(id)
-      .then(data => this.adaptersService.tracks.adapt(data))
+    const data = await this.spotifySdk.tracks.get(id)
+
+    return adapt ? this.adaptersService.tracks.adapt(data) : data
+  }
+
+  public getTracks(ids: string[], adapt: false): Promise<SdkTrack[]>
+  public getTracks(ids: string[], adapt: true): Promise<Track[]>
+
+  async getTracks(ids: string[], adapt = true) {
+    this.spotifySdk = SpotifyApi.withClientCredentials(
+      this.configService.get<string>(Environment.SPOTIFY_CLIENT_ID)!,
+      this.configService.get<string>(Environment.SPOTIFY_CLIENT_SECRET)!
+    )
+
+    const data = await this.spotifySdk.tracks.get(ids)
+
+    return adapt ? this.adaptersService.tracks.adapt(data) : data
   }
 }
