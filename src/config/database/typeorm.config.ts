f004586e9d026b1959443650f57b585ae21ac811
Mnigos <makowskiigor@gmail.com>
2024-03-20 17:04:30 +0100

    refactor(config/typeorm): manually import entities

--- src/config/database/typeorm.config.ts
+++ src/config/database/typeorm.config.ts
@@ -5,6 +5,14 @@ import { ConfigService, registerAs } from '@nestjs/config'
 import { DataSource, DataSourceOptions } from 'typeorm'
 
 import { Environment } from '@config/environment'
+import { User } from '@modules/users'
+import { Profile } from '@modules/profiles'
+import { Album } from '@modules/albums'
+import { Artist } from '@modules/artists'
+import { Track } from '@modules/tracks'
+import { History } from '@modules/history'
+import { Image } from '@modules/images'
+import { HistoryTrack } from '@modules/history/tracks'
 
 const configService = new ConfigService()
 
@@ -15,7 +23,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   username: configService.get(Environment.DATABASE_USERNAME),
   password: configService.get(Environment.DATABASE_PASSWORD),
   database: configService.get(Environment.DATABASE_NAME),
-  entities: ['dist/**/*.entity{.ts,.js}'],
+  entities: [User, Profile, Album, Artist, Track, Image, History, HistoryTrack],
   migrations: ['dist/migrations/*{.ts,.js}'],
   migrationsRun: true,
   autoLoadEntities: true,
