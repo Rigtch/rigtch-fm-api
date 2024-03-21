2db841ed4e491f25ca5727273ff78acd9fde1d40
Mnigos <makowskiigor@gmail.com>
2024-03-21 13:03:52 +0100

    feat(common/mocks): create new factory mocks

---
+++ src/common/mocks/create-query-builder.factory.mock.ts
@@ -0,0 +1,24 @@
+export function createQueryBuilderFactoryMock<T>(entity: T) {
+  return vi.fn().mockReturnValue({
+    orderBy: vi.fn().mockReturnThis(),
+    connection: {
+      createQueryBuilder: vi.fn().mockReturnThis(),
+      select: vi.fn().mockReturnThis(),
+      from: vi.fn().mockReturnThis(),
+      cache: vi.fn().mockReturnThis(),
+      getRawOne: vi.fn().mockReturnThis(),
+      setParameters: vi.fn().mockReturnThis(),
+    },
+    getParameters: vi.fn().mockReturnThis(),
+    getQuery: vi.fn().mockReturnThis(),
+    leftJoinAndSelect: vi.fn().mockReturnThis(),
+    where: vi.fn().mockReturnThis(),
+    limit: vi.fn().mockReturnThis(),
+    offset: vi.fn().mockReturnThis(),
+    skip: vi.fn().mockReturnThis(),
+    take: vi.fn().mockReturnThis(),
+    cache: vi.fn().mockReturnThis(),
+    clone: vi.fn().mockReturnThis(),
+    getMany: vi.fn().mockResolvedValue([entity]),
+  })
+}
