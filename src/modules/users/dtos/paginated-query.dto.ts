d0437be19d4cce0317ec421b1b788e4be55e0147
Mnigos <makowskiigor@gmail.com>
2024-03-21 13:01:03 +0100

    feat(modules/users/dtos): create `PaginatedQuery`

---
+++ src/modules/users/dtos/paginated-query.dto.ts
@@ -0,0 +1,17 @@
+import { Transform } from 'class-transformer'
+import { IsInt, IsOptional, Max, Min } from 'class-validator'
+
+export abstract class PaginatedQuery {
+  @IsInt()
+  @Min(1)
+  @Max(50)
+  @IsOptional()
+  @Transform(({ value }) => (value ? Number.parseInt(value) : 10))
+  limit?: number
+
+  @IsInt()
+  @Min(1)
+  @IsOptional()
+  @Transform(({ value }) => (value ? Number.parseInt(value) : 1))
+  page?: number
+}
