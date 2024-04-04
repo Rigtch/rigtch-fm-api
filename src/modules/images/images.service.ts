4064a77ed09a3221652645e5e85353573717833a
Mnigos <makowskiigor@gmail.com>
2024-04-04 15:04:11 +0200

    feat(modules/images/service): implements `ItemService` absctraction

--- src/modules/images/images.service.ts
+++ src/modules/images/images.service.ts
@@ -2,16 +2,24 @@ import { Injectable } from '@nestjs/common'
 
 import { ImagesRepository } from './images.repository'
 import { CreateImage } from './dtos'
+import { Image } from './image.entity'
+
+import { ItemService } from '@common/abstractions'
 
 @Injectable()
-export class ImagesService {
+export class ImagesService implements ItemService<CreateImage, Image> {
   constructor(private readonly imagesRepository: ImagesRepository) {}
 
   create(data: CreateImage) {
     return this.findOrCreateImage(data)
   }
 
-  findOrCreate(data: CreateImage | CreateImage[]) {
+  public findOrCreate(data: CreateImage): Promise<Image>
+  public findOrCreate(data: CreateImage[]): Promise<Image[]>
+
+  async findOrCreate(
+    data: CreateImage | CreateImage[]
+  ): Promise<Image | Image[]> {
     if (Array.isArray(data)) return this.findOrCreateImages(data)
 
     return this.findOrCreateImage(data)
@@ -25,8 +33,8 @@ export class ImagesService {
     return this.imagesRepository.createImage(image)
   }
 
-  async findOrCreateImages(images?: CreateImage[]) {
-    if (!images || images.length === 0) return []
+  async findOrCreateImages(images: CreateImage[]) {
+    if (images.length === 0) return []
 
     return Promise.all(
       images.map(image => {
