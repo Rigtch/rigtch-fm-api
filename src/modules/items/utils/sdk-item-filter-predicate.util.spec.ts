e101985cee69a9cccc8df046075b2ef3af1beebe
Mnigos <makowskiigor@gmail.com>
2024-05-16 14:49:51 +0200

    feat(modules/items/utils): create `sdkItemFilterPredicate`

---
+++ src/modules/items/utils/sdk-item-filter-predicate.util.spec.ts
@@ -0,0 +1,25 @@
+import { Item } from '../types'
+
+import { sdkItemFilterPredicate } from './sdk-item-filter-predicate.util'
+
+const itemFactoryMock = (externalId: string): Item => ({ externalId, id: '' })
+
+describe('sdkItemFilterPredicate', () => {
+  const items = [itemFactoryMock('1'), itemFactoryMock('2')]
+
+  it('should return true if item with externalId equal to id exists', () => {
+    const id = '1'
+
+    const result = sdkItemFilterPredicate(id, items)
+
+    expect(result).toBeFalsy()
+  })
+
+  it('should return false if item with externalId equal to id does not exist', () => {
+    const id = '3'
+
+    const result = sdkItemFilterPredicate(id, items)
+
+    expect(result).toBeTruthy()
+  })
+})
