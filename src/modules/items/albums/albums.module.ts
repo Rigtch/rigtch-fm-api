10af552fe003c541c5f80bbb65269984ada7db33
Mnigos <makowskiigor@gmail.com>
2024-05-23 13:10:28 +0200

    chore(modules/items): remove unnecessary imports

--- src/modules/items/albums/albums.module.ts
+++ src/modules/items/albums/albums.module.ts
@@ -6,19 +6,10 @@ import { AlbumsRepository } from './albums.repository'
 import { AlbumsController } from './albums.controller'
 import { AlbumsService } from './albums.service'
 
-import { SpotifyModule } from '@modules/spotify'
-import { ImagesModule } from '@modules/items/images'
-import { ArtistsModule } from '@modules/items/artists'
 import { TracksModule } from '@modules/items/tracks'
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Album]),
-    forwardRef(() => TracksModule),
-    forwardRef(() => ArtistsModule),
-    SpotifyModule,
-    ImagesModule,
-  ],
+  imports: [TypeOrmModule.forFeature([Album]), forwardRef(() => TracksModule)],
   providers: [AlbumsRepository, AlbumsService],
   controllers: [AlbumsController],
   exports: [AlbumsRepository, AlbumsService],
