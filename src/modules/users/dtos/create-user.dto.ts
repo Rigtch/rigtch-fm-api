564adc0869ad15063a20fcaf86c077a7bf734a8f
Mnigos <makowskiigor@gmail.com>
2024-05-27 19:10:43 +0200

    refactor(modules/users/dtos/create-user): remove `ApiProperty` decorators

--- src/modules/users/dtos/create-user.dto.ts
+++ src/modules/users/dtos/create-user.dto.ts
@@ -1,13 +1,8 @@
-import { ApiProperty } from '@nestjs/swagger'
-
 import { User } from '../user.entity'
 
 import { Profile } from '@modules/profiles'
 
 export abstract class CreateUser implements Omit<User, 'id'> {
-  @ApiProperty()
   readonly profile: Profile
-
-  @ApiProperty()
   readonly refreshToken: string
 }
