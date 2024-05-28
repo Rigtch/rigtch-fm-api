9095fb5890803ebfe08e90cbcb855f37a8244036
Mnigos <makowskiigor@gmail.com>
2024-05-28 16:17:53 +0200

    chore(environment): remove `JWT_SECRET` variable

--- src/config/environment/environment.schema.ts
+++ src/config/environment/environment.schema.ts
@@ -8,7 +8,6 @@ export const environmentSchema = Joi.object({
   SPOTIFY_BASE_URL: Joi.string().required(),
   SPOTIFY_ACCOUNTS_URL: Joi.string().required(),
   CLIENT_CALLBACK_URL: Joi.string().required(),
-  JWT_SECRET: Joi.string().required(),
   HISTORY_FETCHING_INTERVAL: Joi.string().default('1h'),
   HISTORY_FETCHING_DELAY: Joi.string().default('2m'),
   DATABASE_HOST: Joi.string().required(),
