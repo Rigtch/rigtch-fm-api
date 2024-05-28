87d4923c5fb753e1470ce6cb6adb2d0a8d8ed379
Mnigos <makowskiigor@gmail.com>
2024-05-28 13:01:57 +0200

    style(modules/albums/dtos/create-artist): fix dependency cycle

--- src/modules/items/albums/dtos/create-album.dto.ts
+++ src/modules/items/albums/dtos/create-album.dto.ts
@@ -1,4 +1,3 @@
-import { ApiProperty } from '@nestjs/swagger'
 import {
   IsArray,
   IsDate,
@@ -8,51 +7,42 @@ import {
   IsOptional,
   IsString,
 } from 'class-validator'
-import { Page } from '@spotify/web-api-ts-sdk'
+import type { Page } from '@spotify/web-api-ts-sdk'
 
-import { Album } from '../album.entity'
+import type { Album } from '../album.entity'
 
-import { Artist } from '@modules/items/artists'
-import { Track } from '@modules/items/tracks'
-import {
+import type { Artist } from '@modules/items/artists'
+import type {
   SdkAlbum,
   SdkArtist,
   SdkImage,
   SdkSimplifiedTrack,
 } from '@common/types/spotify'
-import { Image } from '@modules/items/images'
+import type { Image } from '@modules/items/images'
 
 export abstract class CreateAlbum implements Omit<Album, 'id'> {
   @IsString()
-  @ApiProperty()
   readonly externalId: string
 
   @IsString()
-  @ApiProperty()
   readonly name: string
 
   @IsString()
-  @ApiProperty()
   readonly albumType: string
 
   @IsInt()
-  @ApiProperty({ type: Number })
   readonly totalTracks: number
 
   @IsString()
-  @ApiProperty()
   readonly href: string
 
   @IsDate()
-  @ApiProperty({ type: Date })
   readonly releaseDate: Date
 
   @IsArray()
-  @ApiProperty({ type: Image, isArray: true })
   readonly images: Image[]
 
   @IsArray()
-  @ApiProperty({ type: Artist, isArray: true })
   readonly artists: Artist[]
 }
 
@@ -74,40 +64,31 @@ export abstract class SdkCreateAlbum
     >
 {
   @IsString()
-  @ApiProperty()
   readonly id: string
 
   @IsString()
-  @ApiProperty()
   readonly name: string
 
   @IsString()
-  @ApiProperty()
   readonly album_type: string
 
   @IsInt()
-  @ApiProperty({ type: Number })
   readonly total_tracks: number
 
-  @ApiProperty()
   @IsObject()
   readonly external_urls: {
     readonly spotify: string
   }
 
   @IsDateString()
-  @ApiProperty()
   readonly release_date: string
 
   @IsOptional()
   @IsArray()
-  @ApiProperty({ type: Image, isArray: true })
   readonly images: SdkImage[]
 
   @IsArray()
-  @ApiProperty({ type: Artist, isArray: true })
   readonly artists: SdkArtist[]
 
-  @ApiProperty({ type: Track, isArray: true })
   readonly tracks: Page<SdkSimplifiedTrack>
 }
