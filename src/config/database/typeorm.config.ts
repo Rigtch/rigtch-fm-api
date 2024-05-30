386869606b1f5eca0f2904871ee3c1094002bc23
Mnigos <makowskiigor@gmail.com>
2024-05-30 12:44:44 +0200

    style(config/database/typeorm): destructurize environment variables

--- src/config/database/typeorm.config.ts
+++ src/config/database/typeorm.config.ts
@@ -16,13 +16,21 @@ import { HistoryTrack } from '@modules/history/tracks'
 
 const configService = new ConfigService()
 
+const {
+  DATABASE_HOST,
+  DATABASE_PORT,
+  DATABASE_USERNAME,
+  DATABASE_PASSWORD,
+  DATABASE_NAME,
+} = Environment
+
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: configService.get(Environment.DATABASE_HOST),
-  port: configService.get(Environment.DATABASE_PORT),
-  username: configService.get(Environment.DATABASE_USERNAME),
-  password: configService.get(Environment.DATABASE_PASSWORD),
-  database: configService.get(Environment.DATABASE_NAME),
+  host: configService.get(DATABASE_HOST),
+  port: configService.get(DATABASE_PORT),
+  username: configService.get(DATABASE_USERNAME),
+  password: configService.get(DATABASE_PASSWORD),
+  database: configService.get(DATABASE_NAME),
   entities: [User, Profile, Album, Artist, Track, Image, HistoryTrack],
   migrations,
   migrationsRun: true,
