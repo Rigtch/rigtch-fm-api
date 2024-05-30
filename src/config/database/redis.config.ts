4adbb460ea1d6f66396cf531f2ebebcbd598a51f
Mnigos <makowskiigor@gmail.com>
2024-05-30 12:45:50 +0200

    chore(config/database/redis): use variables from `configService`

--- src/config/database/redis.config.ts
+++ src/config/database/redis.config.ts
@@ -1,9 +1,17 @@
-import { registerAs } from '@nestjs/config'
+import { ConfigService, registerAs } from '@nestjs/config'
 import type { RedisOptions } from 'ioredis'
 
+import { Environment } from '@config/environment'
+
+const configService = new ConfigService()
+
+const { REDIS_HOST, REDIS_PORT, REDIS_USER, REDIS_PASSWORD } = Environment
+
 export const redisConfig: RedisOptions = {
-  host: 'localhost',
-  port: 6379,
+  host: configService.get(REDIS_HOST),
+  port: configService.get(REDIS_PORT),
+  username: configService.get(REDIS_USER),
+  password: configService.get(REDIS_PASSWORD),
 }
 
 export const redis = registerAs('redis', () => redisConfig)
