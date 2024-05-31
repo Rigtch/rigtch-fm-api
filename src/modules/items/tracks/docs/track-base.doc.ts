0246eb83bac3f4a87b53cf2cb9b76f973a2d42ea
Mnigos <makowskiigor@gmail.com>
2024-05-31 10:24:05 +0200

    docs(modules/items/tracks/docs): update docs for `TrackBaseDocument`

--- src/modules/items/tracks/docs/track-base.doc.ts
+++ src/modules/items/tracks/docs/track-base.doc.ts
@@ -1,9 +1,18 @@
-import { OmitType } from '@nestjs/swagger'
+import { ApiProperty, OmitType } from '@nestjs/swagger'
 
 import { Track } from '../track.entity'
+
+import { Artist } from '@modules/items/artists'
 
 export class TrackBaseDocument extends OmitType(Track, [
   'album',
   'artists',
   'historyTracks',
-]) {}
+]) {
+  @ApiProperty({
+    type: [Artist],
+    description:
+      'The artists who performed the track. Each artist object includes an externalId to get more detailed information about the artist.',
+  })
+  artists: Artist[]
+}
--- src/modules/items/tracks/docs/track.doc.ts
+++ src/modules/items/tracks/docs/track.doc.ts
@@ -2,7 +2,6 @@ import { ApiProperty } from '@nestjs/swagger'
 
 import { TrackBaseDocument } from './track-base.doc'
 
-import { Artist } from '@modules/items/artists'
 import { AlbumBaseDocument } from '@modules/items/albums/docs/album-base.doc'
 
 export class TrackDocument extends TrackBaseDocument {
@@ -12,11 +11,4 @@ export class TrackDocument extends TrackBaseDocument {
       'The album on which the track appears. The album object includes an externalId to get full information about the album.',
   })
   album: AlbumBaseDocument
-
-  @ApiProperty({
-    type: [Artist],
-    description:
-      'The artists who performed the track. Each artist object includes an externalId to get more detailed information about the artist.',
-  })
-  artists: Artist[]
 }
