71de5e7ba71984dc310dac20459827a921f4ca8e
Mnigos <makowskiigor@gmail.com>
2024-06-05 11:58:50 +0200

    refactor(modules/items/albums/controller): use `nestjs-paginate` and sort images

--- src/modules/items/albums/albums.controller.spec.ts
+++ src/modules/items/albums/albums.controller.spec.ts
@@ -1,20 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { NotFoundException } from '@nestjs/common'
-import { paginate } from 'nestjs-typeorm-paginate'
+import { PaginateQuery } from 'nestjs-paginate'
+import { MockInstance } from 'vitest'
 
 import { AlbumsController } from './albums.controller'
-import {
-  AlbumsRepository,
-  albumsSimplifiedRelations,
-} from './albums.repository'
+import { AlbumsRepository } from './albums.repository'
 
 import {
   albumEntityMock,
-  generatePaginatedResponseFactoryMock,
-  paginatedResponseMockImplementation,
+  createQueryBuilderFactoryMock,
+  createQueryBuilderMockImplementation,
 } from '@common/mocks'
-
-vi.mock('nestjs-typeorm-paginate')
 
 describe('AlbumsController', () => {
   let moduleRef: TestingModule
@@ -28,7 +24,7 @@ describe('AlbumsController', () => {
         {
           provide: AlbumsRepository,
           useValue: {
-            findAlbumById: vi.fn(),
+            createQueryBuilder: createQueryBuilderFactoryMock(albumEntityMock),
           },
         },
       ],
@@ -47,101 +43,74 @@ describe('AlbumsController', () => {
   })
 
   describe('getAlbums', () => {
-    const paginateSpy = vi
-      .mocked(paginate)
-      .mockImplementation(paginatedResponseMockImplementation(albumEntityMock))
+    let paginateQueryMock: PaginateQuery
+
+    beforeEach(() => {
+      paginateQueryMock = {
+        path: '',
+      }
+    })
 
     test('should get all albums', async () => {
-      const paginatedResponseMock =
-        generatePaginatedResponseFactoryMock(albumEntityMock)
+      const response = await albumsController.getAlbums(paginateQueryMock)
 
-      const response = await albumsController.getAlbums({})
-
-      expect(response).toEqual(paginatedResponseMock)
-      expect(response.items.length).toEqual(10)
-      expect(paginateSpy).toHaveBeenCalledWith(
-        albumsRepository,
-        {
-          limit: 10,
-          page: 1,
-        },
-        {
-          relations: albumsSimplifiedRelations,
-        }
-      )
+      expect(response.data).toEqual(albumEntityMock)
+      expect(response.meta.itemsPerPage).toEqual(10)
+      expect(response.meta.currentPage).toEqual(1)
     })
 
     test('should get all albums with limit', async () => {
       const limit = 50
 
-      const paginatedResponseMock = generatePaginatedResponseFactoryMock(
-        albumEntityMock,
-        limit
-      )
+      paginateQueryMock.limit = limit
 
-      const response = await albumsController.getAlbums({ limit })
+      const response = await albumsController.getAlbums(paginateQueryMock)
 
-      expect(response).toEqual(paginatedResponseMock)
-      expect(response.items.length).toEqual(limit)
-      expect(paginateSpy).toHaveBeenCalledWith(
-        albumsRepository,
-        {
-          limit,
-          page: 1,
-        },
-        {
-          relations: albumsSimplifiedRelations,
-        }
-      )
+      expect(response.data).toEqual(albumEntityMock)
+      expect(response.meta.itemsPerPage).toEqual(limit)
+      expect(response.meta.currentPage).toEqual(1)
     })
 
     test('should get all albums with page', async () => {
       const page = 2
 
-      const paginatedResponseMock = generatePaginatedResponseFactoryMock(
-        albumEntityMock,
-        undefined,
-        page
-      )
+      paginateQueryMock.page = page
 
-      const response = await albumsController.getAlbums({ page })
+      const response = await albumsController.getAlbums(paginateQueryMock)
 
-      expect(response).toEqual(paginatedResponseMock)
-      expect(response.items.length).toEqual(10)
-      expect(paginateSpy).toHaveBeenCalledWith(
-        albumsRepository,
-        {
-          limit: 10,
-          page,
-        },
-        {
-          relations: albumsSimplifiedRelations,
-        }
-      )
+      expect(response.data).toEqual(albumEntityMock)
+      expect(response.meta.itemsPerPage).toEqual(10)
+      expect(response.meta.currentPage).toEqual(page)
     })
   })
 
   describe('getAlbumById', () => {
     const id = 'id'
 
+    let createQueryBuilderSpy: MockInstance
+
+    beforeEach(() => {
+      createQueryBuilderSpy = vi.spyOn(albumsRepository, 'createQueryBuilder')
+    })
+
     test('should get album', async () => {
-      const findAlbumByIdSpy = vi
-        .spyOn(albumsRepository, 'findAlbumById')
-        .mockResolvedValue(albumEntityMock)
+      createQueryBuilderSpy.mockReturnValue(
+        createQueryBuilderMockImplementation(albumEntityMock)
+      )
 
       expect(await albumsController.getAlbumById(id)).toEqual(albumEntityMock)
-      expect(findAlbumByIdSpy).toHaveBeenCalledWith(id)
+      expect(createQueryBuilderSpy).toHaveBeenCalledWith('album')
     })
 
     test('should throw not found exception', async () => {
-      const findAlbumByIdSpy = vi
-        .spyOn(albumsRepository, 'findAlbumById')
-        .mockResolvedValue(null)
+      createQueryBuilderSpy.mockReturnValue(
+        createQueryBuilderMockImplementation(null)
+      )
 
       await expect(albumsController.getAlbumById(id)).rejects.toThrowError(
         NotFoundException
       )
-      expect(findAlbumByIdSpy).toHaveBeenCalledWith(id)
+      expect(createQueryBuilderSpy).toHaveBeenCalledWith('album')
     })
   })
 })
