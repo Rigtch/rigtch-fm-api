f753db4e0cd444afcecc3d052361ee43c02dfb48
Mnigos <makowskiigor@gmail.com>
2024-06-05 11:58:12 +0200

    refactor(modules/items/albums/docs): move `artists` to `AlbumBaseDocument`

--- src/modules/items/albums/docs/album.doc.ts
+++ src/modules/items/albums/docs/album.doc.ts
@@ -2,7 +2,6 @@ import { ApiProperty } from '@nestjs/swagger'
 
 import { AlbumBaseDocument } from './album-base.doc'
 
-import { Artist } from '@modules/items/artists'
 import { TrackBaseDocument } from '@modules/items/tracks/docs'
 
 export class AlbumDocument extends AlbumBaseDocument {
@@ -11,11 +10,4 @@ export class AlbumDocument extends AlbumBaseDocument {
     description: 'The tracks of the album.',
   })
   tracks?: TrackBaseDocument[]
-
-  @ApiProperty({
-    type: [Artist],
-    description:
-      'The artists of the album. Each artist object includes a link in href to more detailed information about the artist.',
-  })
-  artists: Artist[]
 }
