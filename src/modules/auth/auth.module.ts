9a48e1dd4afdaf799fa99af8e62aac10e1dcc1d7
Mnigos <makowskiigor@gmail.com>
2024-06-05 09:01:35 +0200

    refactor(modules/auth): remove `AuthService`

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,7 +1,5 @@
 import { Module, forwardRef } from '@nestjs/common'
 
-import { AuthService } from './auth.service'
-
 import { UsersModule } from '@modules/users'
 import { ProfilesModule } from '@modules/profiles'
 import { SpotifyModule } from '@modules/spotify'
@@ -14,7 +12,5 @@ import { HistoryModule } from '@modules/history'
     HistoryModule,
     forwardRef(() => UsersModule),
   ],
-  providers: [AuthService],
-  exports: [AuthService],
 })
 export class AuthModule {}
