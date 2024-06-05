7961b258962ac1cff33e647b0e5abc206dbb7f79
Mnigos <makowskiigor@gmail.com>
2024-06-05 09:03:12 +0200

    refactor(modules/auth/config): remove `spotifyAuthorizationScopes

--- src/modules/auth/config/spotify-authorization-scopes.ts
+++
@@ -1,8 +0,0 @@
-export const spotifyAuthorizationScopes = [
-  'user-read-playback-state',
-  'user-modify-playback-state',
-  'user-read-recently-played',
-  'user-follow-read',
-  'user-top-read',
-  'user-read-currently-playing',
-]
