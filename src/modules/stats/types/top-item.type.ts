0af7596ba02a48df7a6a23e21b76c973ab50b75e
Mnigos <makowskiigor@gmail.com>
2024-07-03 08:27:36 +0200

    feat(modules/stats/types): create `TopItem` type'

---
+++ src/modules/stats/types/top-item.type.ts
@@ -0,0 +1,15 @@
+import type { Album } from '@modules/items/albums'
+import type { Artist } from '@modules/items/artists'
+import type { Track } from '@modules/items/tracks'
+
+export type TopItem<T extends Artist | Album | Track> =
+  | ({
+      item: T
+    } & {
+      playTime: number
+      plays?: never
+    })
+  | {
+      plays: number
+      playTime?: never
+    }
