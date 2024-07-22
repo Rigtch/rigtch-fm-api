1a5d020157ec5c319522a0e97b11e805899c49d2
Mnigos <makowskiigor@gmail.com>
2024-07-22 12:16:06 +0200

    fix(common/mocks/context): make `request` param type `DeepPartial`

--- src/common/mocks/context.factory.mock.ts
+++ src/common/mocks/context.factory.mock.ts
@@ -1,8 +1,9 @@
 import { ExecutionContext } from '@nestjs/common'
 import { Request } from 'express'
+import { DeepPartial } from 'typeorm'
 import { mock } from 'vitest-mock-extended'
 
-export function contextFactoryMock(request: Partial<Request>) {
+export function contextFactoryMock(request: DeepPartial<Request>) {
   return mock<ExecutionContext>({
     switchToHttp: vi.fn().mockReturnValue({
       getRequest: vi.fn().mockReturnValue(request),
