bf498e1ea0525beda278228d001f40431a6304fc
Mnigos <makowskiigor@gmail.com>
2024-07-22 10:22:23 +0200

    feat(modules/history/utils/synchronize-job-id): enchance jobid with repeatable possibility

--- src/modules/history/utils/synchronize-job-id.factory.util.ts
+++ src/modules/history/utils/synchronize-job-id.factory.util.ts
@@ -1,8 +1,18 @@
 import type { UserId } from '../types'
 
 export type Timestamp = number
-export type JobId = `synchronize-history-${UserId}-${Timestamp}`
+export type RepeatableJobId = `synchronize-history-${UserId}-repeatable`
+export type TimestampJobId = `synchronize-history-${UserId}-${Timestamp}`
 
-export function synchronizeJobIdFactory(id: UserId): JobId {
-  return `synchronize-history-${id}-${Date.now()}`
+export function synchronizeJobIdFactory(
+  id: UserId,
+  repeatable?: false
+): TimestampJobId
+export function synchronizeJobIdFactory(
+  id: UserId,
+  repeatable: true
+): RepeatableJobId
+
+export function synchronizeJobIdFactory(id: UserId, repeatable = false) {
+  return `synchronize-history-${id}-${repeatable ? 'repeatable' : Date.now()}`
 }
