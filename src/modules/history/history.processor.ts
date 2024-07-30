51c1fb48f1ee5e82ba27e6daaa7cea2c3d779966
Mnigos <makowskiigor@gmail.com>
2024-07-30 12:39:12 +0200

    fix(history/processor): catch spotify `Refresh token revoked` error

--- src/modules/history/history.processor.ts
+++ src/modules/history/history.processor.ts
@@ -21,39 +21,50 @@ export class HistoryProcessor extends WorkerHost {
   }
 
   async process({ data: user }: Job<User>) {
-    const accessToken = await this.spotifyService.auth.token({
-      refreshToken: user.refreshToken,
-    })
+    try {
+      const accessToken = await this.spotifyService.auth.token({
+        refreshToken: user.refreshToken,
+      })
 
-    const { items: playHistory } =
-      await this.spotifyService.player.getRecentlyPlayedTracks(
-        accessToken,
-        50,
-        {},
-        false
-      )
-
-    const lastHistoryTrack =
-      await this.historyTracksRepository.findLastHistoryTrackByUser(user.id)
-
-    if (lastHistoryTrack) {
-      const latestTrackIndex = playHistory.findIndex(
-        ({ track, played_at }) =>
-          track.id === lastHistoryTrack.track.externalId &&
-          new Date(played_at).getTime() === lastHistoryTrack.playedAt.getTime()
-      )
-
-      const latestPlayHistory = playHistory.filter((_, index) =>
-        latestTrackIndex === -1 ? true : index < latestTrackIndex
-      )
-
-      if (latestPlayHistory.length > 0)
-        await this.historyTracksService.create(latestPlayHistory, user)
-      else
-        this.logger.log(
-          `No new tracks found for user: ${user.profile.displayName} - skipping synchronization`
+      const { items: playHistory } =
+        await this.spotifyService.player.getRecentlyPlayedTracks(
+          accessToken,
+          50,
+          {},
+          false
         )
-    } else await this.historyTracksService.create(playHistory, user)
+
+      const lastHistoryTrack =
+        await this.historyTracksRepository.findLastHistoryTrackByUser(user.id)
+
+      if (lastHistoryTrack) {
+        const latestTrackIndex = playHistory.findIndex(
+          ({ track, played_at }) =>
+            track.id === lastHistoryTrack.track.externalId &&
+            new Date(played_at).getTime() ===
+              lastHistoryTrack.playedAt.getTime()
+        )
+
+        const latestPlayHistory = playHistory.filter((_, index) =>
+          latestTrackIndex === -1 ? true : index < latestTrackIndex
+        )
+
+        if (latestPlayHistory.length > 0)
+          await this.historyTracksService.create(latestPlayHistory, user)
+        else
+          this.logger.log(
+            `No new tracks found for user: ${user.profile.displayName} - skipping synchronization`
+          )
+      } else await this.historyTracksService.create(playHistory, user)
+    } catch (error) {
+      if (
+        'error_description' in error &&
+        error.error_description === 'Refresh token revoked'
+      )
+        return
+
+      throw error
+    }
   }
 
   @OnWorkerEvent('error')
