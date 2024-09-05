c6c494ee0e31d3c78119a59d83025be0fd02bfc2
Mnigos <makowskiigor@gmail.com>
2024-09-05 10:20:24 +0200

    feat(auth): pass authentication check if `params.id` is `PUBLIC_USER_ID`

--- src/modules/users/guards/check-is-current-user.guard.ts
+++ src/modules/users/guards/check-is-current-user.guard.ts
@@ -5,15 +5,23 @@ import {
   Injectable,
 } from '@nestjs/common'
 import { Request } from 'express'
+import { ConfigService } from '@nestjs/config'
 
 import { SpotifyService } from '@modules/spotify'
+import { Environment } from '@config/environment'
 
 @Injectable()
 export class CheckIsCurrentUserGuard implements CanActivate {
-  constructor(private readonly spotifyService: SpotifyService) {}
+  constructor(
+    private readonly spotifyService: SpotifyService,
+    private readonly configService: ConfigService
+  ) {}
 
   async canActivate(context: ExecutionContext) {
     const { headers, user } = context.switchToHttp().getRequest<Request>()
+
+    if (user!.id === this.configService.get<string>(Environment.PUBLIC_USER_ID))
+      return true
 
     const accessToken = headers.authorization?.slice(7)
 
