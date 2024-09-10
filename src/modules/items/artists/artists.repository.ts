04c3acda44d59725d04fc39cfda7f79a08f3313f
Mnigos <makowskiigor@gmail.com>
2024-09-10 21:08:34 +0200

    refactor(modules/items/artists): remove unused methods

--- src/modules/items/artists/artists.repository.ts
+++ src/modules/items/artists/artists.repository.ts
@@ -1,38 +1,11 @@
-import { DataSource, In, Repository } from 'typeorm'
 import { Injectable } from '@nestjs/common'
+import { DataSource, Repository } from 'typeorm'
 
 import { Artist } from './artist.entity'
-import { CreateArtist } from './dtos'
 
 @Injectable()
 export class ArtistsRepository extends Repository<Artist> {
   constructor(private readonly dataSource: DataSource) {
     super(Artist, dataSource.createEntityManager())
-  }
-
-  findArtists() {
-    return this.find()
-  }
-
-  findArtistByExternalId(externalId: string) {
-    return this.findOne({ where: { externalId } })
-  }
-
-  findArtistById(id: string) {
-    return this.findOne({ where: { id } })
-  }
-
-  findArtistByName(name: string) {
-    return this.findOne({ where: { name } })
-  }
-
-  findArtistsByExternalIds(externalIds: string[]) {
-    return this.find({ where: { externalId: In(externalIds) } })
-  }
-
-  async createArtist(artistToCreate: CreateArtist) {
-    const newArtist = this.create(artistToCreate)
-
-    return this.save(newArtist)
   }
 }
