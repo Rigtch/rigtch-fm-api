e510b1f69fcb92361008a12a3e869a320d53e4b4
Mnigos <makowskiigor@gmail.com>
2024-09-10 15:27:53 +0200

    feat(modules/items/artists/service): create `updateOrCreateManyInTransaction` method

--- src/modules/items/artists/artists.service.ts
+++ src/modules/items/artists/artists.service.ts
@@ -1,20 +1,34 @@
 import { Injectable } from '@nestjs/common'
-import { DataSource, In } from 'typeorm'
+import { DataSource, EntityManager, In } from 'typeorm'
 
-import { CreateArtist, SdkCreateArtist } from './dtos'
 import { Artist } from './artist.entity'
+import { CreateArtist, SdkCreateArtist } from './dtos'
 
-import { Image } from '@modules/items/images'
+import { removeDuplicates } from '@common/utils'
+import { Image, ImagesService } from '@modules/items/images'
 
 @Injectable()
 export class ArtistsService {
-  constructor(private readonly dataSource: DataSource) {}
+  constructor(
+    private readonly dataSource: DataSource,
+    private readonly imagesService: ImagesService
+  ) {}
 
   public updateOrCreate(data: SdkCreateArtist): Promise<Artist>
-  public updateOrCreate(data: SdkCreateArtist[]): Promise<Artist[]>
+  public updateOrCreate(
+    data: SdkCreateArtist[],
+    manager?: EntityManager
+  ): Promise<Artist[]>
 
-  async updateOrCreate(data: SdkCreateArtist | SdkCreateArtist[]) {
-    if (Array.isArray(data)) return this.updateOrCreateMany(data)
+  async updateOrCreate(
+    data: SdkCreateArtist | SdkCreateArtist[],
+    manager?: EntityManager
+  ) {
+    if (Array.isArray(data)) {
+      if (manager) return this.updateOrCreateManyInTransaction(data, manager)
+
+      return this.updateOrCreateMany(data)
+    }
 
     return this.updateOrCreateOne(data)
   }
@@ -67,5 +81,58 @@ export class ArtistsService {
 
   private async updateOrCreateMany(artists: SdkCreateArtist[]) {
     return Promise.all(artists.map(artist => this.updateOrCreateOne(artist)))
+  }
+
+  private async updateOrCreateManyInTransaction(
+    sdkArtists: SdkCreateArtist[],
+    manager: EntityManager
+  ) {
+    const artistsExternalIds = removeDuplicates(sdkArtists.map(({ id }) => id))
+
+    const foundArtists = await manager.findBy(Artist, {
+      externalId: In(artistsExternalIds),
+    })
+    const artistsToCreate = sdkArtists.filter(
+      ({ id }) => !foundArtists.some(({ externalId }) => id === externalId)
+    )
+
+    const artistsEntities: Artist[] = []
+
+    if (artistsToCreate.length === 0) return foundArtists
+
+    for (const {
+      id,
+      name,
+      external_urls: { spotify: href },
+      genres,
+      popularity,
+      followers,
+      images,
+    } of artistsToCreate) {
+      const createdImages = await this.imagesService.findOrCreate(
+        images,
+        manager
+      )
+
+      const artistToCreate: Omit<CreateArtist, 'images'> = {
+        externalId: id,
+        name,
+        href,
+        genres,
+        popularity,
+        followers: followers.total,
+      }
+
+      const artistEntity = manager.create(Artist, {
+        ...artistToCreate,
+        images: createdImages,
+      })
+
+      artistsEntities.push(artistEntity)
+    }
+
+    const createdArtists = await manager.save(artistsEntities)
+
+    return [...foundArtists, ...createdArtists]
   }
 }
