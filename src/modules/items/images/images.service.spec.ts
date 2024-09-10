75d36975166a16b8b10456e8e3346fd2a7d3f2ff
Mnigos <makowskiigor@gmail.com>
2024-09-10 17:35:15 +0200

    refactor(modules/items/images): remove unused functionalities

--- src/modules/items/images/images.service.spec.ts
+++ src/modules/items/images/images.service.spec.ts
@@ -1,53 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { DataSource, EntityManager, In } from 'typeorm'
+import { EntityManager, In } from 'typeorm'
 import { MockInstance } from 'vitest'
 import { mock, MockProxy } from 'vitest-mock-extended'
 
-import { ImagesService } from './images.service'
-import { ImagesRepository } from './images.repository'
 import { Image } from './image.entity'
+import { ImagesService } from './images.service'
 
-import {
-  entityManagerFactoryMock,
-  imageMock,
-  imagesMock,
-  sdkImageMock,
-  sdkImagesMock,
-  transactionFactoryMock,
-} from '@common/mocks'
-import { EntityManagerCreateMockInstance } from '@common/types/mocks'
+import { entityManagerFactoryMock } from '@common/mocks'
 import { SdkImage } from '@common/types/spotify'
 
 describe('ImagesService', () => {
   let moduleRef: TestingModule
-  let entityManagerMock: EntityManager
   let imagesService: ImagesService
-  let imagesRepository: ImagesRepository
+  let entityManagerMock: EntityManager
 
   beforeEach(async () => {
     entityManagerMock = entityManagerFactoryMock()
 
     moduleRef = await Test.createTestingModule({
-      providers: [
-        ImagesService,
-        {
-          provide: ImagesRepository,
-          useValue: {
-            findImageByUrl: vi.fn(),
-            createImage: vi.fn(),
-          },
-        },
-        {
-          provide: DataSource,
-          useValue: {
-            transaction: transactionFactoryMock(entityManagerMock),
-          },
-        },
-      ],
+      providers: [ImagesService],
     }).compile()
 
     imagesService = moduleRef.get(ImagesService)
-    imagesRepository = moduleRef.get(ImagesRepository)
   })
 
   afterEach(() => {
@@ -58,158 +32,88 @@ describe('ImagesService', () => {
     expect(imagesService).toBeDefined()
   })
 
-  test('create', async () => {
-    const createImageSpy = vi
-      .spyOn(imagesRepository, 'createImage')
-      .mockResolvedValue(imageMock)
-
-    expect(await imagesService.create(sdkImageMock)).toEqual(imageMock)
-    expect(createImageSpy).toHaveBeenCalledWith(sdkImageMock)
-  })
-
   describe('findOrCreate', () => {
-    describe('findOrCreateOne', () => {
-      let findOneBySpy: MockInstance
+    const imagesUrls = ['url1', 'url2', 'url3']
 
-      beforeEach(() => {
-        findOneBySpy = vi.spyOn(entityManagerMock, 'findOneBy')
-      })
+    let sdkImagesMock: MockProxy<SdkImage[]>
+    let imagesMock: MockProxy<Image[]>
 
-      test('should find image if exists', async () => {
-        findOneBySpy.mockResolvedValue(imageMock)
+    let findBySpy: MockInstance
+    let createSpy: MockInstance
+    let saveSpy: MockInstance
 
-        expect(await imagesService.findOrCreate(sdkImageMock)).toEqual(
-          imageMock
-        )
-        expect(findOneBySpy).toHaveBeenCalledWith(Image, {
-          url: sdkImageMock.url,
+    beforeEach(() => {
+      sdkImagesMock = Array.from({ length: 3 }, (_, index) =>
+        mock<SdkImage>({
+          url: imagesUrls[index],
         })
-      })
-
-      test('should create image if not found', async () => {
-        findOneBySpy.mockResolvedValue(null)
-        const createSpy = (
-          vi.spyOn(
-            entityManagerMock,
-            'create'
-          ) as EntityManagerCreateMockInstance
-        ).mockReturnValue(imageMock)
-        const saveSpy = vi
-          .spyOn(entityManagerMock, 'save')
-          .mockResolvedValue(imageMock)
-
-        expect(await imagesService.findOrCreate(sdkImageMock)).toEqual(
-          imageMock
-        )
-        expect(findOneBySpy).toHaveBeenCalledWith(Image, {
-          url: sdkImageMock.url,
+      )
+      imagesMock = Array.from({ length: 3 }, (_, index) =>
+        mock<Image>({
+          url: imagesUrls[index],
         })
-        expect(createSpy).toHaveBeenCalledWith(Image, sdkImageMock)
-        expect(saveSpy).toHaveBeenCalledWith(imageMock)
-      })
+      )
+
+      findBySpy = vi.spyOn(entityManagerMock, 'findBy')
+      createSpy = vi.spyOn(entityManagerMock, 'create')
+      saveSpy = vi.spyOn(entityManagerMock, 'save')
     })
 
-    describe('findOrCreateMany', () => {
-      test('should return empty array if no images', async () => {
-        expect(await imagesService.findOrCreate([])).toEqual([])
-      })
+    test('should return empty array if no images', async () => {
+      expect(await imagesService.findOrCreate([], entityManagerMock)).toEqual(
+        []
+      )
 
-      test('should find or create images', async () => {
-        const findOrCreateOneSpy = vi
-          .spyOn(imagesService as never, 'findOrCreateOne')
-          .mockResolvedValue(imageMock)
-
-        expect(await imagesService.findOrCreate(sdkImagesMock)).toEqual(
-          imagesMock
-        )
-        expect(findOrCreateOneSpy).toHaveBeenCalledWith(sdkImageMock)
-        expect(findOrCreateOneSpy).toHaveBeenCalledTimes(sdkImagesMock.length)
-      })
+      expect(findBySpy).not.toHaveBeenCalled()
+      expect(createSpy).not.toHaveBeenCalled()
+      expect(saveSpy).not.toHaveBeenCalled()
     })
 
-    describe('findOrCreateManyInTransaction', () => {
-      const imagesUrls = ['url1', 'url2', 'url3']
+    test('should find all images and does not create any', async () => {
+      findBySpy.mockResolvedValue(imagesMock)
 
-      let sdkImagesMock: MockProxy<SdkImage[]>
-      let imagesMock: MockProxy<Image[]>
+      expect(
+        await imagesService.findOrCreate(sdkImagesMock, entityManagerMock)
+      ).toEqual(imagesMock)
 
-      let findBySpy: MockInstance
-      let createSpy: MockInstance
-      let saveSpy: MockInstance
-
-      beforeEach(() => {
-        sdkImagesMock = Array.from({ length: 3 }, (_, index) =>
-          mock<SdkImage>({
-            url: imagesUrls[index],
-          })
-        )
-        imagesMock = Array.from({ length: 3 }, (_, index) =>
-          mock<Image>({
-            url: imagesUrls[index],
-          })
-        )
-
-        findBySpy = vi.spyOn(entityManagerMock, 'findBy')
-        createSpy = vi.spyOn(entityManagerMock, 'create')
-        saveSpy = vi.spyOn(entityManagerMock, 'save')
+      expect(findBySpy).toHaveBeenCalledWith(Image, {
+        url: In(imagesMock.map(image => image.url)),
       })
+      expect(findBySpy).toHaveBeenCalledTimes(1)
+      expect(createSpy).not.toHaveBeenCalled()
+      expect(saveSpy).not.toHaveBeenCalled()
+    })
 
-      test('should return empty array if no images', async () => {
-        expect(await imagesService.findOrCreate([], entityManagerMock)).toEqual(
-          []
-        )
+    test('should not find any images and create all', async () => {
+      findBySpy.mockResolvedValue([])
+      createSpy.mockReturnValue(imagesMock)
+      saveSpy.mockResolvedValue(imagesMock)
 
-        expect(findBySpy).not.toHaveBeenCalled()
-        expect(createSpy).not.toHaveBeenCalled()
-        expect(saveSpy).not.toHaveBeenCalled()
+      expect(
+        await imagesService.findOrCreate(sdkImagesMock, entityManagerMock)
+      ).toEqual(imagesMock)
+
+      expect(findBySpy).toHaveBeenCalledWith(Image, {
+        url: In(imagesMock.map(image => image.url)),
       })
+      expect(createSpy).toHaveBeenCalledWith(Image, sdkImagesMock)
+      expect(saveSpy).toHaveBeenCalledWith(imagesMock)
+    })
 
-      test('should find all images and does not create any', async () => {
-        findBySpy.mockResolvedValue(imagesMock)
+    test('should find some images and create the rest', async () => {
+      findBySpy.mockResolvedValue([imagesMock[0], imagesMock[1]])
+      createSpy.mockReturnValue([imagesMock[2]])
+      saveSpy.mockResolvedValue([imagesMock[2]])
 
-        expect(
-          await imagesService.findOrCreate(sdkImagesMock, entityManagerMock)
-        ).toEqual(imagesMock)
+      expect(
+        await imagesService.findOrCreate(sdkImagesMock, entityManagerMock)
+      ).toEqual(imagesMock)
 
-        expect(findBySpy).toHaveBeenCalledWith(Image, {
-          url: In(imagesMock.map(image => image.url)),
-        })
-        expect(findBySpy).toHaveBeenCalledTimes(1)
-        expect(createSpy).not.toHaveBeenCalled()
-        expect(saveSpy).not.toHaveBeenCalled()
+      expect(findBySpy).toHaveBeenCalledWith(Image, {
+        url: In(imagesMock.map(image => image.url)),
       })
-
-      test('should not find any images and create all', async () => {
-        findBySpy.mockResolvedValue([])
-        createSpy.mockReturnValue(imagesMock)
-        saveSpy.mockResolvedValue(imagesMock)
-
-        expect(
-          await imagesService.findOrCreate(sdkImagesMock, entityManagerMock)
-        ).toEqual(imagesMock)
-
-        expect(findBySpy).toHaveBeenCalledWith(Image, {
-          url: In(imagesMock.map(image => image.url)),
-        })
-        expect(createSpy).toHaveBeenCalledWith(Image, sdkImagesMock)
-        expect(saveSpy).toHaveBeenCalledWith(imagesMock)
-      })
-
-      test('should find some images and create the rest', async () => {
-        findBySpy.mockResolvedValue([imagesMock[0], imagesMock[1]])
-        createSpy.mockReturnValue([imagesMock[2]])
-        saveSpy.mockResolvedValue([imagesMock[2]])
-
-        expect(
-          await imagesService.findOrCreate(sdkImagesMock, entityManagerMock)
-        ).toEqual(imagesMock)
-
-        expect(findBySpy).toHaveBeenCalledWith(Image, {
-          url: In(imagesMock.map(image => image.url)),
-        })
-        expect(createSpy).toHaveBeenCalledWith(Image, [sdkImagesMock[2]])
-        expect(saveSpy).toHaveBeenCalledWith([imagesMock[2]])
-      })
+      expect(createSpy).toHaveBeenCalledWith(Image, [sdkImagesMock[2]])
+      expect(saveSpy).toHaveBeenCalledWith([imagesMock[2]])
     })
   })
 })
--- src/modules/items/images/images.service.ts
+++ src/modules/items/images/images.service.ts
@@ -1,64 +1,12 @@
 import { Injectable } from '@nestjs/common'
-import { DataSource, EntityManager, In } from 'typeorm'
+import { EntityManager, In } from 'typeorm'
 
-import { ImagesRepository } from './images.repository'
 import { CreateImage } from './dtos'
 import { Image } from './image.entity'
 
 @Injectable()
 export class ImagesService {
-  constructor(
-    private readonly imagesRepository: ImagesRepository,
-    private readonly dataSource: DataSource
-  ) {}
-
-  create(newImage: CreateImage) {
-    return this.imagesRepository.createImage(newImage)
-  }
-
-  public findOrCreate(data: CreateImage): Promise<Image>
-  public findOrCreate(
-    data: CreateImage[],
-    manager?: EntityManager
-  ): Promise<Image[]>
-
-  async findOrCreate(
-    data: CreateImage | CreateImage[],
-    manager?: EntityManager
-  ): Promise<Image | Image[]> {
-    if (Array.isArray(data)) {
-      if (manager) return this.findOrCreateManyInTransaction(data, manager)
-
-      return this.findOrCreateMany(data)
-    }
-
-    return this.findOrCreateOne(data)
-  }
-
-  private async findOrCreateOne(image: CreateImage) {
-    return this.dataSource.transaction(async manager => {
-      const foundImage = await manager.findOneBy(Image, {
-        url: image.url,
-      })
-
-      if (foundImage) return foundImage
-
-      const imageEntity = manager.create(Image, image)
-
-      return manager.save(imageEntity)
-    })
-  }
-
-  private async findOrCreateMany(images: CreateImage[]) {
-    if (images.length === 0) return [] as Image[]
-
-    return Promise.all(images.map(image => this.findOrCreateOne(image)))
-  }
-
-  private async findOrCreateManyInTransaction(
-    sdkImages: CreateImage[],
-    manager: EntityManager
-  ) {
+  async findOrCreate(sdkImages: CreateImage[], manager: EntityManager) {
     if (sdkImages.length === 0) return [] as Image[]
 
     const foundImages = await manager.findBy(Image, {
