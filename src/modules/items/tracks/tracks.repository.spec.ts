eeae10a4f31d55e44957c6a18c670500ba1ef47f
Mnigos <makowskiigor@gmail.com>
2024-09-10 21:12:55 +0200

    refactor(modules/items/tracks/repository): remove unused methods

--- src/modules/items/tracks/tracks.repository.spec.ts
+++ src/modules/items/tracks/tracks.repository.spec.ts
@@ -1,15 +1,7 @@
-import { DataSource, In } from 'typeorm'
 import { Test, TestingModule } from '@nestjs/testing'
+import { DataSource } from 'typeorm'
 
-import { TracksRepository, tracksRelations } from './tracks.repository'
-
-import {
-  albumEntityMock,
-  artistEntitiesMock,
-  sdkTrackMock,
-  trackEntitiesMock,
-  trackEntityMock,
-} from '@common/mocks'
+import { TracksRepository } from './tracks.repository'
 
 describe('TracksRepository', () => {
   let moduleRef: TestingModule
@@ -37,101 +29,5 @@ describe('TracksRepository', () => {
 
   test('should be defined', () => {
     expect(tracksRepository).toBeDefined()
-  })
-
-  test('should find tracks', async () => {
-    const findSpy = vi
-      .spyOn(tracksRepository, 'find')
-      .mockResolvedValue(trackEntitiesMock)
-
-    expect(await tracksRepository.findTracks()).toEqual(trackEntitiesMock)
-    expect(findSpy).toHaveBeenCalledWith({
-      relations: tracksRelations,
-    })
-  })
-
-  test('should find track by external id', async () => {
-    const externalId = 'externalId'
-
-    const findOneSpy = vi
-      .spyOn(tracksRepository, 'findOne')
-      .mockResolvedValue(trackEntityMock)
-
-    expect(await tracksRepository.findTrackByExternalId(externalId)).toEqual(
-      trackEntityMock
-    )
-    expect(findOneSpy).toHaveBeenCalledWith({
-      where: { externalId },
-      relations: tracksRelations,
-    })
-  })
-
-  test('should find track by id', async () => {
-    const id = 'id'
-
-    const findOneSpy = vi
-      .spyOn(tracksRepository, 'findOne')
-      .mockResolvedValue(trackEntityMock)
-
-    expect(await tracksRepository.findTrackById(id)).toEqual(trackEntityMock)
-    expect(findOneSpy).toHaveBeenCalledWith({
-      where: { id },
-      relations: tracksRelations,
-    })
-  })
-
-  test('should find track by name', async () => {
-    const name = 'name'
-
-    const findOneSpy = vi
-      .spyOn(tracksRepository, 'findOne')
-      .mockResolvedValue(trackEntityMock)
-
-    expect(await tracksRepository.findTrackByName(name)).toEqual(
-      trackEntityMock
-    )
-    expect(findOneSpy).toHaveBeenCalledWith({
-      where: { name },
-      relations: tracksRelations,
-    })
-  })
-
-  test('should find tracks by external ids', async () => {
-    const externalIds = ['id1', 'id2']
-
-    const findSpy = vi
-      .spyOn(tracksRepository, 'find')
-      .mockResolvedValue(trackEntitiesMock)
-
-    expect(await tracksRepository.findTracksByExternalIds(externalIds)).toEqual(
-      trackEntitiesMock
-    )
-    expect(findSpy).toHaveBeenCalledWith({
-      where: { externalId: In(externalIds) },
-      relations: tracksRelations,
-    })
-  })
-
-  test('should create track', async () => {
-    const createSpy = vi
-      .spyOn(tracksRepository, 'create')
-      .mockReturnValue(trackEntityMock)
-    const saveSpy = vi
-      .spyOn(tracksRepository, 'save')
-      .mockResolvedValue(trackEntityMock)
-
-    expect(
-      await tracksRepository.createTrack({
-        ...sdkTrackMock,
-        externalId: sdkTrackMock.id,
-        duration: sdkTrackMock.duration_ms,
-        trackNumber: sdkTrackMock.track_number,
-        discNumber: sdkTrackMock.disc_number,
-        album: albumEntityMock,
-        artists: artistEntitiesMock,
-      })
-    ).toEqual(trackEntityMock)
-    expect(createSpy).toHaveBeenCalled()
-    expect(saveSpy).toHaveBeenCalledWith(trackEntityMock)
   })
 })
