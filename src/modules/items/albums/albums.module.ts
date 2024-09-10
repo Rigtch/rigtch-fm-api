3170d3af8f058f4298ea6995c834ac048b057c8a
Mnigos <makowskiigor@gmail.com>
2024-09-10 17:36:37 +0200

    refactor(modules/items/albums): remove unused functionalities

--- src/modules/items/albums/albums.module.ts
+++ src/modules/items/albums/albums.module.ts
@@ -1,21 +1,20 @@
-import { Module, forwardRef } from '@nestjs/common'
+import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 
-import { ArtistsModule } from '../artists'
-import { ImagesModule } from '../images'
-
 import { Album } from './album.entity'
-import { AlbumsRepository } from './albums.repository'
 import { AlbumsController } from './albums.controller'
+import { AlbumsRepository } from './albums.repository'
 import { AlbumsService } from './albums.service'
 
+import { ImagesModule } from '@modules/items/images'
+import { ArtistsModule } from '@modules/items/artists'
 import { TracksModule } from '@modules/items/tracks'
 import { SpotifyModule } from '@modules/spotify'
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Album]),
-    forwardRef(() => TracksModule),
+    TracksModule,
     SpotifyModule,
     ArtistsModule,
     ImagesModule,
--- src/modules/items/albums/albums.service.ts
+++ src/modules/items/albums/albums.service.ts
@@ -1,123 +1,24 @@
-import { Inject, Injectable, forwardRef } from '@nestjs/common'
-import { DataSource, EntityManager, In } from 'typeorm'
+import { Injectable } from '@nestjs/common'
+import { EntityManager, In } from 'typeorm'
 
-import { CreateAlbum, SdkCreateAlbum } from './dtos'
 import { Album } from './album.entity'
+import { CreateAlbum, SdkCreateAlbum } from './dtos'
 import { ReleaseDatePrecision } from './enums'
 
-import { TracksService } from '@modules/items/tracks/tracks.service'
-import { Artist, ArtistsService } from '@modules/items/artists'
-import { Image, ImagesService } from '@modules/items/images'
 import { removeDuplicates } from '@common/utils'
+import { ArtistsService } from '@modules/items/artists'
+import { ImagesService } from '@modules/items/images'
+import { TracksService } from '@modules/items/tracks/tracks.service'
 
 @Injectable()
 export class AlbumsService {
   constructor(
-    private readonly dataSource: DataSource,
-    @Inject(forwardRef(() => TracksService))
     private readonly tracksService: TracksService,
     private readonly artistsService: ArtistsService,
     private readonly imagesService: ImagesService
   ) {}
 
-  public updateOrCreate(data: SdkCreateAlbum): Promise<Album>
-  public updateOrCreate(
-    data: SdkCreateAlbum[],
-    manager?: EntityManager
-  ): Promise<Album[]>
-
-  async updateOrCreate(
-    data: SdkCreateAlbum | SdkCreateAlbum[],
-    manager?: EntityManager
-  ) {
-    if (Array.isArray(data)) {
-      if (manager) return this.updateOrCreateManyInTransaction(data, manager)
-
-      return this.updateOrCreateMany(data)
-    }
-
-    return this.updateOrCreateOne(data)
-  }
-
-  private async updateOrCreateOne(fetchedAlbum: SdkCreateAlbum) {
-    const {
-      id,
-      name,
-      release_date,
-      release_date_precision,
-      label,
-      copyrights,
-      genres,
-      album_type: albumType,
-      total_tracks: totalTracks,
-      external_urls: { spotify: href },
-      images: fetchedAlbumImages,
-      artists: fetchedAlbumArtists,
-      tracks: fetchedAlbumTracks,
-    } = fetchedAlbum
-
-    const albumToCreate: Omit<CreateAlbum, 'images' | 'artists' | 'tracks'> = {
-      name,
-      externalId: id,
-      href,
-      albumType,
-      releaseDate: new Date(release_date),
-      releaseDatePrecision: release_date_precision as ReleaseDatePrecision,
-      label,
-      copyrights: copyrights.map(({ text }) => text),
-      genres,
-      totalTracks,
-    }
-
-    const createdAlbum = await this.dataSource.transaction(async manager => {
-      const foundAlbum = await manager.findOneBy(Album, { externalId: id })
-
-      if (foundAlbum) {
-        await manager.update(Album, { externalId: id }, albumToCreate)
-
-        const foundCreatedAlbum = await manager.findOneBy(Album, {
-          externalId: id,
-        })
-
-        return foundCreatedAlbum!
-      }
-
-      const images = await manager.findBy(Image, {
-        url: In(fetchedAlbumImages.map(image => image.url)),
-      })
-      const artists = await manager.findBy(Artist, {
-        externalId: In(fetchedAlbumArtists.map(({ id }) => id)),
-      })
-
-      const albumEntity = manager.create(Album, {
-        ...albumToCreate,
-        images,
-        artists,
-      })
-
-      return manager.save(albumEntity)
-    })
-
-    await this.tracksService.updateOrCreate(
-      fetchedAlbumTracks.items.map(track => ({
-        ...track,
-        album: fetchedAlbum,
-      }))
-    )
-
-    return createdAlbum
-  }
-
-  private async updateOrCreateMany(fetchedAlbums: SdkCreateAlbum[]) {
-    return Promise.all(
-      fetchedAlbums.map(album => this.updateOrCreateOne(album))
-    )
-  }
-
-  private async updateOrCreateManyInTransaction(
-    sdkAlbums: SdkCreateAlbum[],
-    manager: EntityManager
-  ) {
+  async updateOrCreate(sdkAlbums: SdkCreateAlbum[], manager: EntityManager) {
     const albumsExternalIds = removeDuplicates(sdkAlbums.map(({ id }) => id))
 
     const foundAlbums = await manager.findBy(Album, {
