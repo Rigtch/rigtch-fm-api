2ccecb88463c5dd0b175a37e2f02e6f8ab710995
Mnigos <makowskiigor@gmail.com>
2024-09-10 15:28:37 +0200

    feat(modules/items/tracks/service): create `updateOrCreateManyInTransaction` method

--- src/modules/items/tracks/tracks.service.spec.ts
+++ src/modules/items/tracks/tracks.service.spec.ts
@@ -1,29 +1,42 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { MockInstance } from 'vitest'
 import { DataSource, EntityManager, In } from 'typeorm'
+import { MockInstance } from 'vitest'
+import { MockProxy } from 'vitest-mock-extended'
 
-import { TracksService } from './tracks.service'
 import { Track } from './track.entity'
+import { TracksService } from './tracks.service'
 
-import { Album } from '@modules/items/albums'
-import { Artist } from '@modules/items/artists'
 import {
-  sdkTrackMock,
-  trackEntityMock,
   albumEntityMock,
   artistEntitiesMock,
+  artistsMock,
   entityManagerFactoryMock,
-  transactionFactoryMock,
+  sdkArtistsMock,
   sdkCreateTrackMock,
   sdkCreateTracksMock,
+  sdkSimplifiedAlbumMock,
+  sdkSimplifiedArtistsMock,
+  sdkSimplifiedTrackMock,
+  sdkTrackMock,
   trackEntitiesMock,
+  trackEntityMock,
+  transactionFactoryMock,
 } from '@common/mocks'
-import { EntityManagerCreateMockInstance } from '@common/types/mocks'
+import {
+  EntityManagerCreateMockInstance,
+  GetItemsMockInstance,
+} from '@common/types/mocks'
+import { SdkArtist, SdkTrack } from '@common/types/spotify'
+import { Album } from '@modules/items/albums'
+import { Artist, ArtistsService } from '@modules/items/artists'
+import { SpotifyService } from '@modules/spotify'
 
 describe('TracksService', () => {
   let moduleRef: TestingModule
   let entityManagerMock: EntityManager
   let tracksService: TracksService
+  let artistsService: ArtistsService
+  let spotifyService: SpotifyService
 
   beforeEach(async () => {
     entityManagerMock = entityManagerFactoryMock()
@@ -37,10 +50,26 @@ describe('TracksService', () => {
             transaction: transactionFactoryMock(entityManagerMock),
           },
         },
+        {
+          provide: ArtistsService,
+          useValue: {
+            updateOrCreate: vi.fn(),
+          },
+        },
+        {
+          provide: SpotifyService,
+          useValue: {
+            artists: {
+              get: vi.fn(),
+            },
+          },
+        },
       ],
     }).compile()
 
     tracksService = moduleRef.get(TracksService)
+    artistsService = moduleRef.get(ArtistsService)
+    spotifyService = moduleRef.get(SpotifyService)
   })
 
   afterEach(() => {
@@ -128,6 +157,180 @@ describe('TracksService', () => {
         )
         expect(updateOrCreateOneSpy).toHaveBeenCalledWith(sdkCreateTrackMock)
         expect(updateOrCreateOneSpy).toHaveBeenCalledTimes(5)
+      })
+    })
+
+    describe('updateOrCreateManyInTransaction', () => {
+      const tracksExternalIds = ['id1', 'id2', 'id3']
+      const customSdkSimplifiedArtistsMock = sdkSimplifiedArtistsMock
+        .slice(0, 3)
+        .map((artist, index) => ({
+          ...artist,
+          id: tracksExternalIds[index],
+        }))
+      const customSdkArtistsMock = sdkArtistsMock
+        .slice(0, 3)
+        .map((artist, index) => ({
+          ...artist,
+          id: tracksExternalIds[index],
+        }))
+      const customArtistsMock = artistsMock
+        .slice(0, 3)
+        .map((artist, index) => ({
+          externalId: tracksExternalIds[index],
+          ...artist,
+        }))
+
+      let sdkTracksMock: MockProxy<SdkTrack>[]
+      let tracksMock: MockProxy<Track>[]
+
+      let findBySpy: MockInstance
+      let createSpy: MockInstance
+      let saveSpy: MockInstance
+      let artistsUpdateOrCreateSpy: MockInstance
+      let getSpotifyArtistsSpy: GetItemsMockInstance<SdkArtist>
+
+      beforeEach(() => {
+        sdkTracksMock = Array.from(
+          { length: 3 },
+          (_, index) =>
+            ({
+              ...sdkSimplifiedTrackMock,
+              id: tracksExternalIds[index],
+              artists: customSdkSimplifiedArtistsMock,
+              album: sdkSimplifiedAlbumMock,
+            }) as SdkTrack
+        )
+        tracksMock = Array.from(
+          { length: 3 },
+          (_, index) =>
+            ({
+              externalId: tracksExternalIds[index],
+              artists: customArtistsMock,
+              album: albumEntityMock,
+            }) as Track
+        )
+
+        findBySpy = vi.spyOn(entityManagerMock, 'findBy')
+        createSpy = vi.spyOn(entityManagerMock, 'create')
+        saveSpy = vi.spyOn(entityManagerMock, 'save')
+        artistsUpdateOrCreateSpy = vi.spyOn(artistsService, 'updateOrCreate')
+        getSpotifyArtistsSpy = vi.spyOn(spotifyService.artists, 'get')
+      })
+
+      test('should find all tracks and does not create any', async () => {
+        findBySpy.mockResolvedValue(tracksMock)
+
+        expect(
+          await tracksService.updateOrCreate(
+            sdkTracksMock,
+            entityManagerMock,
+            albumEntityMock
+          )
+        ).toEqual(tracksMock)
+
+        expect(findBySpy).toHaveBeenCalledWith(Track, {
+          externalId: In(tracksExternalIds),
+        })
+        expect(findBySpy).toHaveBeenCalledTimes(1)
+        expect(createSpy).not.toHaveBeenCalled()
+        expect(saveSpy).not.toHaveBeenCalled()
+        expect(artistsUpdateOrCreateSpy).not.toHaveBeenCalled()
+      })
+
+      test('should not find any tracks and create all', async () => {
+        findBySpy.mockResolvedValueOnce([]).mockResolvedValue(customArtistsMock)
+        createSpy.mockImplementation((_, { externalId }) =>
+          tracksMock.find(track => track.externalId === externalId)
+        )
+        saveSpy.mockResolvedValue(tracksMock)
+
+        expect(
+          await tracksService.updateOrCreate(
+            sdkTracksMock,
+            entityManagerMock,
+            albumEntityMock
+          )
+        ).toEqual(tracksMock)
+
+        expect(findBySpy).toHaveBeenCalledWith(Track, {
+          externalId: In(tracksExternalIds),
+        })
+        expect(findBySpy).toHaveBeenCalledWith(Artist, {
+          externalId: In(tracksExternalIds),
+        })
+        expect(createSpy).toHaveBeenCalledWith(Track, expect.anything())
+        expect(createSpy).toHaveBeenCalledTimes(3)
+        expect(saveSpy).toHaveBeenCalledWith(tracksMock)
+        expect(artistsUpdateOrCreateSpy).not.toHaveBeenCalled()
+      })
+
+      test('should not find any tracks and create all with some artists', async () => {
+        findBySpy
+          .mockResolvedValueOnce([])
+          .mockResolvedValue([customArtistsMock[0], customArtistsMock[1]])
+        getSpotifyArtistsSpy.mockResolvedValue([customSdkArtistsMock[2]])
+        artistsUpdateOrCreateSpy.mockResolvedValue([customArtistsMock[2]])
+        createSpy.mockImplementation((_, { externalId }) =>
+          tracksMock.find(track => track.externalId === externalId)
+        )
+        saveSpy.mockResolvedValue(tracksMock)
+
+        expect(
+          await tracksService.updateOrCreate(
+            sdkTracksMock,
+            entityManagerMock,
+            albumEntityMock
+          )
+        ).toEqual(tracksMock)
+
+        expect(findBySpy).toHaveBeenCalledWith(Track, {
+          externalId: In(tracksExternalIds),
+        })
+        expect(findBySpy).toHaveBeenCalledWith(Artist, {
+          externalId: In(tracksExternalIds),
+        })
+        expect(getSpotifyArtistsSpy).toHaveBeenCalledWith(
+          tracksExternalIds.slice(2, 3),
+          false
+        )
+        expect(artistsUpdateOrCreateSpy).toHaveBeenCalledWith(
+          customSdkArtistsMock.slice(2, 3),
+          entityManagerMock
+        )
+        expect(createSpy).toHaveBeenCalledWith(Track, expect.anything())
+        expect(createSpy).toHaveBeenCalledTimes(3)
+        expect(saveSpy).toHaveBeenCalledWith(tracksMock)
+      })
+
+      test('should find some tracks and create the rest', async () => {
+        findBySpy
+          .mockResolvedValueOnce([tracksMock[0], tracksMock[1]])
+          .mockResolvedValue(customArtistsMock)
+        createSpy.mockImplementation((_, { externalId }) =>
+          tracksMock.find(track => track.externalId === externalId)
+        )
+        saveSpy.mockResolvedValue([tracksMock[2]])
+
+        expect(
+          await tracksService.updateOrCreate(
+            sdkTracksMock,
+            entityManagerMock,
+            albumEntityMock
+          )
+        ).toEqual(tracksMock)
+
+        expect(findBySpy).toHaveBeenCalledWith(Track, {
+          externalId: In(tracksExternalIds),
+        })
+        expect(findBySpy).toHaveBeenCalledWith(Artist, {
+          externalId: In(tracksExternalIds),
+        })
+        expect(getSpotifyArtistsSpy).not.toHaveBeenCalled()
+        expect(artistsUpdateOrCreateSpy).not.toHaveBeenCalled()
+        expect(createSpy).toHaveBeenCalledWith(Track, expect.anything())
+        expect(createSpy).toHaveBeenCalledTimes(1)
+        expect(saveSpy).toHaveBeenCalledWith([tracksMock[2]])
       })
     })
   })
--- src/modules/items/tracks/tracks.service.ts
+++ src/modules/items/tracks/tracks.service.ts
@@ -1,20 +1,40 @@
 import { Injectable } from '@nestjs/common'
-import { DataSource, In } from 'typeorm'
+import { DataSource, EntityManager, In } from 'typeorm'
 
 import { Track } from './track.entity'
-import { SdkCreateTrack } from './dtos'
+import { CreateTrack, SdkCreateTrack } from './dtos'
 
 import { Album } from '@modules/items/albums/album.entity'
-import { Artist } from '@modules/items/artists'
+import { Artist, ArtistsService } from '@modules/items/artists'
+import { removeDuplicates } from '@common/utils'
+import { SpotifyService } from '@modules/spotify'
 @Injectable()
 export class TracksService {
-  constructor(private readonly dataSource: DataSource) {}
+  constructor(
+    private readonly dataSource: DataSource,
+    private readonly artistsService: ArtistsService,
+    private readonly spotifyService: SpotifyService
+  ) {}
 
   public updateOrCreate(data: SdkCreateTrack): Promise<Track>
   public updateOrCreate(data: SdkCreateTrack[]): Promise<Track[]>
+  public updateOrCreate(
+    data: Omit<SdkCreateTrack, 'album'>[],
+    manager: EntityManager,
+    album: Album
+  ): Promise<Track[]>
 
-  async updateOrCreate(data: SdkCreateTrack | SdkCreateTrack[]) {
-    if (Array.isArray(data)) return this.updateOrCreateMany(data)
+  async updateOrCreate(
+    data: SdkCreateTrack | SdkCreateTrack[] | Omit<SdkCreateTrack, 'album'>[],
+    manager?: EntityManager,
+    album?: Album
+  ) {
+    if (Array.isArray(data)) {
+      if (manager && album)
+        return this.updateOrCreateManyInTransaction(data, manager, album)
+
+      return this.updateOrCreateMany(data as SdkCreateTrack[])
+    }
 
     return this.updateOrCreateOne(data)
   }
@@ -62,5 +82,83 @@ export class TracksService {
     if (tracks.length === 0) return []
 
     return Promise.all(tracks.map(track => this.updateOrCreateOne(track)))
+  }
+
+  private async updateOrCreateManyInTransaction(
+    sdkTracks: Omit<SdkCreateTrack, 'album'>[],
+    manager: EntityManager,
+    album: Album
+  ) {
+    const tracksExternalIds = removeDuplicates(sdkTracks.map(({ id }) => id))
+
+    const foundTracks = await manager.findBy(Track, {
+      externalId: In(tracksExternalIds),
+    })
+    const tracksToCreate = sdkTracks.filter(
+      ({ id }) => !foundTracks.some(({ externalId }) => id === externalId)
+    )
+
+    const tracksEntities: Track[] = []
+
+    if (tracksToCreate.length === 0) return foundTracks
+
+    for (const {
+      id,
+      name,
+      external_urls: { spotify: href },
+      duration_ms,
+      track_number,
+      disc_number,
+      explicit,
+      artists: simplifiedTrackArtists,
+    } of tracksToCreate) {
+      const artistsExternalIds = removeDuplicates(
+        simplifiedTrackArtists.map(({ id }) => id)
+      )
+
+      const foundArtists = await manager.findBy(Artist, {
+        externalId: In(artistsExternalIds),
+      })
+
+      const artists: Artist[] = [...foundArtists]
+
+      if (foundArtists.length !== artistsExternalIds.length) {
+        const artistsToCreateExternalIds = artistsExternalIds.filter(
+          id => !foundArtists.some(({ externalId }) => id === externalId)
+        )
+
+        const sdkArtistsToCreate = await this.spotifyService.artists.get(
+          artistsToCreateExternalIds,
+          false
+        )
+
+        const createdArtists = await this.artistsService.updateOrCreate(
+          sdkArtistsToCreate,
+          manager
+        )
+
+        artists.push(...createdArtists)
+      }
+
+      const trackToCreate: CreateTrack = {
+        name,
+        externalId: id,
+        href,
+        duration: duration_ms,
+        trackNumber: track_number,
+        discNumber: disc_number,
+        explicit,
+        artists,
+        album,
+      }
+
+      const trackEntity = manager.create(Track, trackToCreate)
+
+      tracksEntities.push(trackEntity)
+    }
+
+    const createdTracks = await manager.save(tracksEntities)
+
+    return [...foundTracks, ...createdTracks]
   }
 }
