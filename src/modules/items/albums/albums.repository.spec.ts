56ac8a7ec1d56aa3f3348f572abafd964be78f69
Mnigos <makowskiigor@gmail.com>
2024-09-10 21:07:05 +0200

    refactor(modules/items/albums/repository): remove unused methods

--- src/modules/items/albums/albums.repository.spec.ts
+++ src/modules/items/albums/albums.repository.spec.ts
@@ -1,21 +1,9 @@
-import { DataSource, In } from 'typeorm'
 import { Test, TestingModule } from '@nestjs/testing'
+import { DataSource } from 'typeorm'
 
-import { AlbumsRepository, albumsRelations } from './albums.repository'
-import { ReleaseDatePrecision } from './enums'
-
-import {
-  albumEntityMock,
-  albumMock,
-  albumsEntitiesMock,
-  artistEntitiesMock,
-  imagesMock,
-  sdkAlbumMock,
-} from '@common/mocks'
+import { AlbumsRepository } from './albums.repository'
 
 describe('AlbumsRepository', () => {
-  const externalId = 'externalId'
-
   let moduleRef: TestingModule
   let albumsRepository: AlbumsRepository
 
@@ -41,101 +29,5 @@ describe('AlbumsRepository', () => {
 
   test('should be defined', () => {
     expect(albumsRepository).toBeDefined()
-  })
-
-  test('should find albums', async () => {
-    const findSpy = vi
-      .spyOn(albumsRepository, 'find')
-      .mockResolvedValue(albumsEntitiesMock)
-
-    expect(await albumsRepository.findAlbums()).toEqual(albumsEntitiesMock)
-    expect(findSpy).toHaveBeenCalledWith({ relations: albumsRelations })
-  })
-
-  test('should find albums by external ids', async () => {
-    const externalIds = [externalId]
-
-    const findSpy = vi
-      .spyOn(albumsRepository, 'find')
-      .mockResolvedValue(albumsEntitiesMock)
-
-    expect(await albumsRepository.findAlbumsByExternalIds(externalIds)).toEqual(
-      albumsEntitiesMock
-    )
-    expect(findSpy).toHaveBeenCalledWith({
-      where: { externalId: In(externalIds) },
-      relations: albumsRelations,
-    })
-  })
-
-  test('should find album by external id', async () => {
-    const findOneSpy = vi
-      .spyOn(albumsRepository, 'findOne')
-      .mockResolvedValue(albumEntityMock)
-
-    expect(await albumsRepository.findAlbumByExternalId(externalId)).toEqual(
-      albumEntityMock
-    )
-    expect(findOneSpy).toHaveBeenCalledWith({
-      where: { externalId },
-      relations: albumsRelations,
-    })
-  })
-
-  test('should find album by id', async () => {
-    const id = 'id'
-
-    const findOneSpy = vi
-      .spyOn(albumsRepository, 'findOne')
-      .mockResolvedValue(albumEntityMock)
-
-    expect(await albumsRepository.findAlbumById(id)).toEqual(albumEntityMock)
-    expect(findOneSpy).toHaveBeenCalledWith({
-      where: { id },
-      relations: albumsRelations,
-    })
-  })
-
-  test('should find album by name', async () => {
-    const name = 'name'
-
-    const findOneSpy = vi
-      .spyOn(albumsRepository, 'findOne')
-      .mockResolvedValue(albumEntityMock)
-
-    expect(await albumsRepository.findAlbumByName(name)).toEqual(
-      albumEntityMock
-    )
-    expect(findOneSpy).toHaveBeenCalledWith({
-      where: { name },
-      relations: albumsRelations,
-    })
-  })
-
-  test('should create album', async () => {
-    const createSpy = vi
-      .spyOn(albumsRepository, 'create')
-      .mockReturnValue(albumEntityMock)
-    const saveSpy = vi
-      .spyOn(albumsRepository, 'save')
-      .mockResolvedValue(albumEntityMock)
-
-    expect(
-      await albumsRepository.createAlbum({
-        ...albumMock,
-        artists: artistEntitiesMock,
-        releaseDate: new Date(sdkAlbumMock.release_date),
-        releaseDatePrecision:
-          sdkAlbumMock.release_date_precision as ReleaseDatePrecision,
-        externalId,
-        albumType: sdkAlbumMock.album_type,
-        label: sdkAlbumMock.label,
-        copyrights: sdkAlbumMock.copyrights.map(({ text }) => text),
-        genres: sdkAlbumMock.genres,
-        images: imagesMock,
-      })
-    ).toEqual(albumEntityMock)
-    expect(createSpy).toHaveBeenCalled()
-    expect(saveSpy).toHaveBeenCalledWith(albumEntityMock)
   })
 })
