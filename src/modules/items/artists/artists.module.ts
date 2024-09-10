e86b10be35228a3c409aac0be05a73ecf85ef599
Mnigos <makowskiigor@gmail.com>
2024-09-10 15:27:53 +0200

    feat(modules/items/artists/service): create `updateOrCreateManyInTransaction` method

--- src/modules/items/artists/artists.module.ts
+++ src/modules/items/artists/artists.module.ts
@@ -1,6 +1,8 @@
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Module } from '@nestjs/common'
 
+import { ImagesModule } from '../images'
+
 import { Artist } from './artist.entity'
 import { ArtistsRepository } from './artists.repository'
 import { ArtistsService } from './artists.service'
@@ -8,7 +10,7 @@ import { ArtistsService } from './artists.service'
 import { SpotifyModule } from '@modules/spotify'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Artist]), SpotifyModule],
+  imports: [TypeOrmModule.forFeature([Artist]), SpotifyModule, ImagesModule],
   providers: [ArtistsRepository, ArtistsService],
   exports: [ArtistsRepository, ArtistsService],
 })
--- src/modules/items/artists/artists.service.spec.ts
+++ src/modules/items/artists/artists.service.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { DataSource, EntityManager, In, UpdateResult } from 'typeorm'
-import { mock } from 'vitest-mock-extended'
+import { mock, MockProxy } from 'vitest-mock-extended'
 import { MockInstance } from 'vitest'
 
 import { ArtistsService } from './artists.service'
@@ -12,14 +12,17 @@ import {
   sdkArtistMock,
   transactionFactoryMock,
   entityManagerFactoryMock,
+  sdkImagesMock,
 } from '@common/mocks'
-import { Image } from '@modules/items/images'
+import { Image, ImagesService } from '@modules/items/images'
 import { EntityManagerCreateMockInstance } from '@common/types/mocks'
+import { SdkArtist } from '@common/types/spotify'
 
 describe('ArtistsService', () => {
   let moduleRef: TestingModule
   let entityManagerMock: EntityManager
   let artistsService: ArtistsService
+  let imagesService: ImagesService
 
   beforeEach(async () => {
     entityManagerMock = entityManagerFactoryMock()
@@ -33,10 +36,17 @@ describe('ArtistsService', () => {
             transaction: transactionFactoryMock(entityManagerMock),
           },
         },
+        {
+          provide: ImagesService,
+          useValue: {
+            findOrCreate: vi.fn(),
+          },
+        },
       ],
     }).compile()
 
     artistsService = moduleRef.get(ArtistsService)
+    imagesService = moduleRef.get(ImagesService)
   })
 
   afterEach(() => {
@@ -112,6 +122,106 @@ describe('ArtistsService', () => {
           Array.from({ length: 5 }).map(() => artistEntityMock)
         )
         expect(updateOrCreateOneSpy).toHaveBeenCalledTimes(5)
+      })
+    })
+
+    describe('updateOrCreateManyInTransaction', () => {
+      const artistsExternalIds = ['id1', 'id2', 'id3']
+
+      let sdkArtistsMock: MockProxy<SdkArtist>[]
+      let artistsMock: MockProxy<Artist>[]
+
+      let findBySpy: MockInstance
+      let createSpy: MockInstance
+      let saveSpy: MockInstance
+      let imagesFindOrCreateSpy: MockInstance
+
+      beforeEach(() => {
+        sdkArtistsMock = Array.from({ length: 3 }, (_, index) =>
+          mock<SdkArtist>({
+            id: artistsExternalIds[index],
+            images: sdkImagesMock,
+            type: 'artist',
+          })
+        )
+        artistsMock = Array.from({ length: 3 }, (_, index) =>
+          mock<Artist>({
+            externalId: artistsExternalIds[index],
+            images: imagesMock,
+          })
+        )
+
+        findBySpy = vi.spyOn(entityManagerMock, 'findBy')
+        createSpy = vi.spyOn(entityManagerMock, 'create')
+        saveSpy = vi.spyOn(entityManagerMock, 'save')
+        imagesFindOrCreateSpy = vi.spyOn(imagesService, 'findOrCreate')
+      })
+
+      test('should find all artists and does not create any', async () => {
+        findBySpy.mockResolvedValue(artistsMock)
+
+        expect(
+          await artistsService.updateOrCreate(sdkArtistsMock, entityManagerMock)
+        ).toEqual(artistsMock)
+
+        expect(findBySpy).toHaveBeenCalledWith(Artist, {
+          externalId: In(artistsExternalIds),
+        })
+        expect(findBySpy).toHaveBeenCalledTimes(1)
+        expect(createSpy).not.toHaveBeenCalled()
+        expect(saveSpy).not.toHaveBeenCalled()
+        expect(imagesFindOrCreateSpy).not.toHaveBeenCalled()
+      })
+
+      test('should not find any artists and create all', async () => {
+        findBySpy.mockResolvedValueOnce([])
+        createSpy.mockImplementation((_, { externalId }) =>
+          artistsMock.find(artist => artist.externalId === externalId)
+        )
+        saveSpy.mockResolvedValue(artistsMock)
+        imagesFindOrCreateSpy.mockResolvedValue(imagesMock)
+
+        expect(
+          await artistsService.updateOrCreate(sdkArtistsMock, entityManagerMock)
+        ).toEqual(artistsMock)
+
+        expect(findBySpy).toHaveBeenCalledWith(Artist, {
+          externalId: In(artistsExternalIds),
+        })
+        expect(createSpy).toHaveBeenCalledWith(Artist, expect.anything())
+        expect(createSpy).toHaveBeenCalledTimes(3)
+        expect(saveSpy).toHaveBeenCalledWith(artistsMock)
+        expect(imagesFindOrCreateSpy).toHaveBeenCalledWith(
+          sdkImagesMock,
+          entityManagerMock
+        )
+        expect(imagesFindOrCreateSpy).toHaveBeenCalledTimes(3)
+      })
+
+      test('should find some artists and create the rest', async () => {
+        const foundArtistsMock = [artistsMock[0], artistsMock[1]]
+
+        findBySpy.mockResolvedValueOnce(foundArtistsMock)
+        createSpy.mockImplementation((_, { externalId }) =>
+          artistsMock.find(artist => artist.externalId === externalId)
+        )
+        saveSpy.mockResolvedValue([artistsMock[2]])
+
+        expect(
+          await artistsService.updateOrCreate(sdkArtistsMock, entityManagerMock)
+        ).toEqual(artistsMock)
+
+        expect(findBySpy).toHaveBeenCalledWith(Artist, {
+          externalId: In(artistsExternalIds),
+        })
+        expect(createSpy).toHaveBeenCalledWith(Artist, expect.anything())
+        expect(createSpy).toHaveBeenCalledTimes(1)
+        expect(saveSpy).toHaveBeenCalledWith([artistsMock[2]])
+        expect(imagesFindOrCreateSpy).toHaveBeenCalledWith(
+          sdkImagesMock,
+          entityManagerMock
+        )
+        expect(imagesFindOrCreateSpy).toHaveBeenCalledTimes(1)
       })
     })
   })
