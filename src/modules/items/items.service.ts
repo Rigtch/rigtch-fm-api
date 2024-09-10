cf4edac17491a53b4aa5ac0d99c34b379c0008a9
Mnigos <makowskiigor@gmail.com>
2024-09-10 15:30:14 +0200

    refactor(modules/items/service): seperate queries with proper transactions use

--- src/modules/items/items.service.ts
+++ src/modules/items/items.service.ts
@@ -1,37 +1,21 @@
 import { Injectable } from '@nestjs/common'
-import { In } from 'typeorm'
+import { DataSource, In } from 'typeorm'
 
-import {
-  Album,
-  AlbumsRepository,
-  AlbumsService,
-  albumsSimplifiedRelations,
-} from './albums'
-import { Artist, ArtistsRepository, ArtistsService } from './artists'
-import { Track, TracksRepository, TracksService } from './tracks'
-import { sdkItemFilterPredicate } from './utils'
+import { Album, AlbumsService } from './albums'
+import { Artist, ArtistsService } from './artists'
+import { Track, tracksRelations } from './tracks'
 
 import { removeDuplicates } from '@common/utils'
-import { ImagesService } from '@modules/items/images'
-import {
-  SdkAlbum,
-  SdkArtist,
-  SdkSimplifiedAlbum,
-  SdkTrack,
-} from '@common/types/spotify'
+import { SdkArtist, SdkSimplifiedAlbum, SdkTrack } from '@common/types/spotify'
 import { SpotifyService } from '@modules/spotify'
 
 @Injectable()
 export class ItemsService {
   constructor(
     private readonly albumsService: AlbumsService,
-    private readonly albumsRepository: AlbumsRepository,
     private readonly artistsService: ArtistsService,
-    private readonly artistsRepository: ArtistsRepository,
-    private readonly tracksService: TracksService,
-    private readonly tracksRepository: TracksRepository,
-    private readonly imagesService: ImagesService,
-    private readonly spotifyService: SpotifyService
+    private readonly spotifyService: SpotifyService,
+    private readonly dataSource: DataSource
   ) {}
 
   public findOrCreate(tracks: SdkTrack[]): Promise<Track[]>
@@ -49,128 +33,74 @@ export class ItemsService {
       return this.findOrCreateAlbums(items as SdkSimplifiedAlbum[])
   }
 
-  private async findOrCreateArtists(artists: SdkArtist[]) {
-    const artistsExternalIds = removeDuplicates(artists.map(({ id }) => id))
-
-    const filteredArtists = await this.getFilteredArtists(artistsExternalIds)
-
-    const images = filteredArtists.flatMap(({ images }) => images)
-
-    if (images.length > 0) await this.imagesService.findOrCreate(images)
-
-    await this.artistsService.updateOrCreate(filteredArtists)
-
-    return this.artistsRepository.findArtistsByExternalIds(artistsExternalIds)
-  }
-
-  private async findOrCreateTracks(tracks: SdkTrack[]) {
-    const filteredArtists = await this.getFilteredArtists(
-      removeDuplicates(
-        tracks.flatMap(({ artists }) => artists.map(({ id }) => id))
-      )
-    )
-
-    const filteredAlbums = await this.getFilteredAlbums(
-      removeDuplicates(tracks.map(({ album }) => album.id))
-    )
-
-    const images = [
-      ...filteredArtists.flatMap(({ images }) => images),
-      ...filteredAlbums.flatMap(({ images }) => images),
-    ]
-
-    if (images.length > 0) await this.imagesService.findOrCreate(images)
-    if (filteredArtists.length > 0)
-      await this.artistsService.updateOrCreate(filteredArtists)
-    if (filteredAlbums.length > 0)
-      await this.albumsService.updateOrCreate(filteredAlbums)
-
-    return this.tracksRepository.findTracksByExternalIds(
-      removeDuplicates(tracks.map(({ id }) => id))
-    )
-  }
-
-  private async findOrCreateAlbums(fetchedAlbums: SdkSimplifiedAlbum[]) {
-    const albumsExternalIds = removeDuplicates(
-      fetchedAlbums.map(({ id }) => id)
-    )
-
-    const filteredArtists = await this.getFilteredArtists(
-      removeDuplicates(
-        fetchedAlbums.flatMap(({ artists }) => artists.map(({ id }) => id))
-      )
-    )
-
-    const filteredAlbums = await this.getFilteredAlbums(
-      removeDuplicates(fetchedAlbums.map(({ id }) => id))
-    )
-
-    const images = [
-      ...filteredArtists.flatMap(({ images }) => images),
-      ...filteredAlbums.flatMap(({ images }) => images),
-    ]
-
-    if (images.length > 0) await this.imagesService.findOrCreate(images)
-    if (filteredArtists.length > 0)
-      await this.artistsService.updateOrCreate(filteredArtists)
-    if (filteredAlbums.length > 0)
-      await this.albumsService.updateOrCreate(filteredAlbums)
-
-    return this.albumsRepository.find({
-      where: { externalId: In(albumsExternalIds) },
-      relations: albumsSimplifiedRelations,
+  private findOrCreateArtists(sdkArtists: SdkArtist[]) {
+    return this.dataSource.transaction(async manager => {
+      return this.artistsService.updateOrCreate(sdkArtists, manager)
     })
   }
 
-  private async getFilteredArtists(artistsExternalIds: string[]) {
-    const foundArtists =
-      await this.artistsRepository.findArtistsByExternalIds(artistsExternalIds)
+  private async findOrCreateTracks(tracks: SdkTrack[]) {
+    return this.dataSource.transaction(async manager => {
+      const tracksExternalIds = removeDuplicates(tracks.map(({ id }) => id))
+      const albumsExternalIds = removeDuplicates(
+        tracks.flatMap(({ album }) => album.id)
+      )
 
-    const fetchedArtists = await this.spotifyService.artists.get(
-      artistsExternalIds,
-      false
-    )
-    return fetchedArtists.filter(({ id }) =>
-      sdkItemFilterPredicate(id, foundArtists)
-    )
+      const foundAlbums = await manager.findBy(Album, {
+        externalId: In(albumsExternalIds),
+      })
+      const albumsToCreateExternalIds = albumsExternalIds.filter(
+        id => !foundAlbums.some(({ externalId }) => id === externalId)
+      )
+
+      if (albumsToCreateExternalIds.length === 0)
+        return manager.find(Track, {
+          where: {
+            externalId: In(tracksExternalIds),
+          },
+          relations: tracksRelations,
+        })
+
+      const fetchedAlbums = await this.spotifyService.albums.get(
+        albumsToCreateExternalIds,
+        false
+      )
+
+      await this.albumsService.updateOrCreate(fetchedAlbums, manager)
+
+      return manager.find(Track, {
+        where: {
+          externalId: In(tracksExternalIds),
+        },
+        relations: tracksRelations,
+      })
+    })
   }
 
-  private async getFilteredAlbums(albumsExternalIds: string[]) {
-    const fetchedAlbums = await this.spotifyService.albums.get(
-      albumsExternalIds,
-      false
-    )
-    const foundAlbums = await this.getAlbums(albumsExternalIds, fetchedAlbums)
+  private async findOrCreateAlbums(sdkAlbums: SdkSimplifiedAlbum[]) {
+    return this.dataSource.transaction(async manager => {
+      const albumsExternalIds = removeDuplicates(sdkAlbums.map(({ id }) => id))
 
-    return fetchedAlbums.filter(({ id }) =>
-      sdkItemFilterPredicate(id, foundAlbums)
-    )
-  }
+      const foundAlbums = await manager.findBy(Album, {
+        externalId: In(albumsExternalIds),
+      })
+      const albumsToCreateExternalIds = albumsExternalIds.filter(
+        id => !foundAlbums.some(({ externalId }) => id === externalId)
+      )
 
-  private async getAlbums(
-    albumsExternalIds: string[],
-    fetchedAlbums: SdkAlbum[]
-  ) {
-    const foundAlbums =
-      await this.albumsRepository.findAlbumsByExternalIds(albumsExternalIds)
+      if (albumsToCreateExternalIds.length === 0) return foundAlbums
 
-    for await (const foundAlbum of foundAlbums) {
-      if (foundAlbum.tracks?.length === 0) {
-        const fetchedAlbum = fetchedAlbums.find(
-          ({ id }) => id === foundAlbum.externalId
-        )!
+      const fetchedAlbums = await this.spotifyService.albums.get(
+        albumsToCreateExternalIds,
+        false
+      )
 
-        foundAlbum.tracks = await this.tracksService.updateOrCreate(
-          fetchedAlbum.tracks.items.map(track => ({
-            ...track,
-            album: fetchedAlbum,
-          }))
-        )
+      const createdAlbums = await this.albumsService.updateOrCreate(
+        fetchedAlbums,
+        manager
+      )
 
-        await this.albumsRepository.save(foundAlbum)
-      }
-    }
-
-    return foundAlbums
+      return [...foundAlbums, ...createdAlbums]
+    })
   }
 }
