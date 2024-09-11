5ea426c10e5b89cc62be2cae391ff72f0a02d2a7
Mnigos <makowskiigor@gmail.com>
2024-09-11 12:51:51 +0200

    fix(modules/items/albums): remove subscriber

--- src/modules/items/albums/albums.module.ts
+++ src/modules/items/albums/albums.module.ts
@@ -5,10 +5,9 @@ import { Album } from './album.entity'
 import { AlbumsController } from './albums.controller'
 import { AlbumsRepository } from './albums.repository'
 import { AlbumsService } from './albums.service'
-import { AlbumSubscriber } from './album.subscriber'
 
-import { ImagesModule } from '@modules/items/images'
 import { ArtistsModule } from '@modules/items/artists'
+import { ImagesModule } from '@modules/items/images'
 import { TracksModule } from '@modules/items/tracks'
 import { SpotifyModule } from '@modules/spotify'
 
@@ -20,7 +19,7 @@ import { SpotifyModule } from '@modules/spotify'
     ArtistsModule,
     ImagesModule,
   ],
-  providers: [AlbumsRepository, AlbumsService, AlbumSubscriber],
+  providers: [AlbumsRepository, AlbumsService],
   controllers: [AlbumsController],
   exports: [AlbumsRepository, AlbumsService],
 })
