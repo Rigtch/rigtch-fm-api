84a84c00cd7ac410273826d4881698aa5ce1aecc
Mnigos <makowskiigor@gmail.com>
2024-09-25 18:15:20 +0200

    feat(modules/users/guards): add `FollowUserGuard`

---
+++ src/modules/users/guards/follow-user.guard.ts
@@ -0,0 +1,45 @@
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+} from '@nestjs/common'
+import { Request } from 'express'
+
+import { UsersRepository } from '../users.repository'
+import { User } from '../user.entity'
+
+@Injectable()
+export class FollowUserGuard implements CanActivate {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  async canActivate(context: ExecutionContext) {
+    const {
+      user,
+      body: { followerId },
+    } = context.switchToHttp().getRequest<Request>()
+
+    if (followerId === user!.id)
+      throw new BadRequestException('You cannot follow yourself.')
+
+    const follower: { following: User[] } | null =
+      await this.usersRepository.findOne({
+        where: {
+          id: followerId as string,
+        },
+        relations: {
+          following: true,
+        },
+        select: {
+          following: true,
+        },
+      })
+
+    if (!follower) return false
+
+    if (follower.following.some(({ id }) => id === user!.id))
+      throw new BadRequestException('You are already following this user.')
+
+    return true
+  }
+}
--- src/modules/users/guards/index.ts
+++ src/modules/users/guards/index.ts
@@ -1,2 +1,3 @@
 export * from './validate-user-id.guard'
 export * from './check-is-current-user.guard'
+export * from './follow-user.guard'
